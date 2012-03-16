9ae8a2212d0c85cfa904ea3bf03c0631f6c1369a
Wim Vriend <facetracknoir@gmail.com>
2012-03-16 14:30:06 +0000

    Filter added.
    git-svn-id: svn+ssh://svn.code.sf.net/p/facetracknoir/code@103 19e81ba0-9b1a-49c3-bd6c-561e1906d5fb

---
+++ FTNoIR_Filter_Accela/FTNoIR_Filter_accela.h
@@ -0,0 +1,105 @@
+/********************************************************************************
+* FaceTrackNoIR		This program is a private project of some enthusiastic		*
+*					gamers from Holland, who don't like to pay much for			*
+*					head-tracking.												*
+*																				*
+* Copyright (C) 2012	Wim Vriend (Developing)									*
+*						Ron Hendriks (Researching and Testing)					*
+*																				*
+* Homepage																		*
+*																				*
+* This program is free software; you can redistribute it and/or modify it		*
+* under the terms of the GNU General Public License as published by the			*
+* Free Software Foundation; either version 3 of the License, or (at your		*
+* option) any later version.													*
+*																				*
+* This program is distributed in the hope that it will be useful, but			*
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY	*
+* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for	*
+* more details.																	*
+*																				*
+* You should have received a copy of the GNU General Public License along		*
+* with this program; if not, see <http://www.gnu.org/licenses/>.				*
+*																				*
+********************************************************************************/
+#pragma once
+#ifndef INCLUDED_FTN_FILTER_H
+#define INCLUDED_FTN_FILTER_H
+
+#include "..\ftnoir_filter_base\ftnoir_filter_base.h"
+#include "ui_FTNoIR_FilterControls.h"
+
+class FTNoIR_Filter : public IFilter
+{
+public:
+	FTNoIR_Filter();
+	~FTNoIR_Filter();
+
+	void Release();
+    void Initialize();
+    void StartFilter();
+	void FilterHeadPoseData(THeadPoseData *current_camera_position, THeadPoseData *target_camera_position, THeadPoseData *new_camera_position, bool newTarget);
+
+	void getFullName(QString *strToBeFilled);
+	void getShortName(QString *strToBeFilled);
+	void getDescription(QString *strToBeFilled);
+
+private:
+	void loadSettings();									// Load the settings from the INI-file
+	THeadPoseData newHeadPose;								// Structure with new headpose
+
+	bool	first_run;
+	double kFactor, kFactorTranslation;
+	double kSensitivity, kSensitivityTranslation;
+	double kC1, kC2, kC3, kC4, kC0;
+
+	QString filterFullName;									// Filters' name and description
+	QString filterShortName;
+	QString filterDescription;
+};
+
+//*******************************************************************************************************
+// FaceTrackNoIR Filter Settings-dialog.
+//*******************************************************************************************************
+
+// Widget that has controls for FTNoIR protocol filter-settings.
+class FilterControls: public QWidget, Ui::UICFilterControls, public IFilterDialog
+{
+    Q_OBJECT
+public:
+
+	explicit FilterControls();
+    virtual ~FilterControls();
+	void showEvent ( QShowEvent * event );
+
+	void Release();											// Member functions which are accessible from outside the DLL
+    void Initialize(QWidget *parent, IFilterPtr ptr);
+	void getFullName(QString *strToBeFilled);
+	void getShortName(QString *strToBeFilled);
+	void getDescription(QString *strToBeFilled);
+	void getIcon(QIcon *icon);
+
+private:
+	Ui::UICFilterControls ui;
+	void loadSettings();
+	void save();
+
+	/** helper **/
+	bool settingsDirty;
+
+	QString filterFullName;									// Filters' name and description
+	QString filterShortName;
+	QString filterDescription;
+	IFilterPtr pFilter;										// If the filter was active when the dialog was opened, this will hold a pointer to the Filter instance
+
+private slots:
+	void doOK();
+	void doCancel();
+	void settingChanged() { settingsDirty = true; };
+	void settingChanged( int ) { settingsDirty = true; };
+	void settingChanged( double ) { settingsDirty = true; };
+};
+
+#endif						//INCLUDED_FTN_FILTER_H
+//END
+
