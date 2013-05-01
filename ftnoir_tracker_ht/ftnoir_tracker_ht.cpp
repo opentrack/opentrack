a3e1575724615c2bc79a9bf9dadcba875000ea47
Stanisław Halik <sthalik@misaki.pl>
2013-05-01 08:38:56 +0200

    Use less CPU at exchange for poorer tracking

--- ftnoir_tracker_ht/ftnoir_tracker_ht.cpp
+++ ftnoir_tracker_ht/ftnoir_tracker_ht.cpp
@@ -110,21 +110,21 @@ static void load_settings(ht_config_t* config, Tracker* tracker)
 	config->pyrlk_pyramids = 3;
     config->pyrlk_win_size_w = config->pyrlk_win_size_h = 21;
     config->max_keypoints = 300;
-    config->keypoint_quality = 2;
-    config->keypoint_distance = 1.4;
+    config->keypoint_quality = 9;
+    config->keypoint_distance = 1.9;
     config->keypoint_3distance = 4;
     config->keypoint_9distance = 8;
     //config->force_width = 640;
     //config->force_height = 480;
     config->force_fps = iniFile.value("fps", 0).toInt();
     config->camera_index = iniFile.value("camera-index", -1).toInt();
-    config->ransac_num_iters = 100;
-    config->ransac_max_reprojection_error = 4;
-    config->ransac_max_inlier_error = 5;
+    config->ransac_num_iters = 50;
+    config->ransac_max_reprojection_error = 4.5;
+    config->ransac_max_inlier_error = 6;
     config->ransac_abs_max_mean_error = 8;
     config->ransac_max_mean_error = 3.9;
     config->debug = 1;
-    config->ransac_min_features = 0.84;
+    config->ransac_min_features = 0.75;
     int res = iniFile.value("resolution", 0).toInt();
     if (res < 0 || res >= (int)(sizeof(*resolution_choices) / sizeof(resolution_tuple)))
 		res = 0;
