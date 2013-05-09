080266d8189dd05756e27a9ddd92c4124d8e3ca0
Stanislaw Halik <sthalik@misaki.pl>
2013-05-10 01:13:37 +0200

    Project Evil: set path automatically for Wine

--- ftnoir_protocol_wine/opentrack-wrapper-wine-windows.cxx
+++ ftnoir_protocol_wine/opentrack-wrapper-wine-windows.cxx
@@ -5,3 +5,29 @@
 #include "ftnoir_protocol_ft/fttypes.h"
 #include "compat/compat.h"
 #include "compat/compat.cpp"
+#include <string.h>
+
+void create_registry_key(void) {
+    char dir[8192];
+    
+    if (GetCurrentDirectoryA(8192, dir) < 8192)
+    {
+        HKEY hkpath;
+        if (RegCreateKeyExA(HKEY_CURRENT_USER,
+                            "Software\\NaturalPoint\\NATURALPOINT\\NPClient Location",
+                            0,
+                            NULL,
+                            0,
+                            KEY_ALL_ACCESS,
+                            NULL,
+                            &hkpath,
+                            NULL) == ERROR_SUCCESS)
+        {
+            for (int i = 0; dir[i]; i++)
+                if (dir[i] == '\\')
+                    dir[i] = '/';
+            (void) RegSetValueExA(hkpath, "Path", 0, REG_SZ, (BYTE*) dir, strlen(dir) + 1);
+            RegCloseKey(hkpath);
+        }
+    }
+}
