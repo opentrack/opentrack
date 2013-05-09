7b7e7543d6b6975d31f4c8d53ae18919cd5b7258
Stanislaw Halik <sthalik@misaki.pl>
2013-05-10 01:12:47 +0200

    Once again tweak coefficients

--- ftnoir_tracker_ht/ftnoir_tracker_ht.cpp
+++ ftnoir_tracker_ht/ftnoir_tracker_ht.cpp
@@ -118,10 +118,10 @@ static void load_settings(ht_config_t* config, Tracker* tracker)
     config->force_fps = iniFile.value("fps", 0).toInt();
     config->camera_index = iniFile.value("camera-index", -1).toInt();
     config->ransac_num_iters = 100;
-    config->ransac_max_reprojection_error = 3;
-    config->ransac_max_inlier_error = 3;
+    config->ransac_max_reprojection_error = 3.75;
+    config->ransac_max_inlier_error = 3.75;
     config->ransac_abs_max_mean_error = 7;
-    config->ransac_max_mean_error = 3.8;
+    config->ransac_max_mean_error = 3.7;
     config->debug = 0;
     config->ransac_min_features = 0.85;
     int res = iniFile.value("resolution", 0).toInt();
