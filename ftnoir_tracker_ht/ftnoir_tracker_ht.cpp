730c614636f6ef5a064086b4434797d027edd40e
Stanislaw Halik <sthalik@misaki.pl>
2013-05-09 09:07:07 +0200

    Adjust coefficients for poor lighting

--- ftnoir_tracker_ht/ftnoir_tracker_ht.cpp
+++ ftnoir_tracker_ht/ftnoir_tracker_ht.cpp
@@ -111,20 +111,20 @@ static void load_settings(ht_config_t* config, Tracker* tracker)
     config->pyrlk_win_size_w = config->pyrlk_win_size_h = 21;
     config->max_keypoints = 300;
     config->keypoint_quality = 2;
-    config->keypoint_distance = 2;
+    config->keypoint_distance = 1.5;
     config->keypoint_3distance = 4;
-    config->keypoint_9distance = 9;
+    config->keypoint_9distance = 8;
     //config->force_width = 640;
     //config->force_height = 480;
     config->force_fps = iniFile.value("fps", 0).toInt();
     config->camera_index = iniFile.value("camera-index", -1).toInt();
     config->ransac_num_iters = 100;
-    config->ransac_max_reprojection_error = 3.2;
-    config->ransac_max_inlier_error = 3;
+    config->ransac_max_reprojection_error = 3.5;
+    config->ransac_max_inlier_error = 3.2;
     config->ransac_abs_max_mean_error = 7;
     config->ransac_max_mean_error = 3.15;
     config->debug = 0;
-    config->ransac_min_features = 0.68;
+    config->ransac_min_features = 0.72;
     int res = iniFile.value("resolution", 0).toInt();
     if (res < 0 || res >= (int)(sizeof(*resolution_choices) / sizeof(resolution_tuple)))
 		res = 0;
@@ -132,7 +132,7 @@ static void load_settings(ht_config_t* config, Tracker* tracker)
 	config->force_width = r.width;
     config->force_height = r.height;
     config->user_landmarks = false; //iniFile.value("use-bashed-coords").toBool();
-    config->flandmark_delay = 65;
+    config->flandmark_delay = 90;
 #if 0
     if (config->user_landmarks)
     {
