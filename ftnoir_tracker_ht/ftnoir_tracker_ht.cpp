c2fd8a5fa223eeccc5b788a85b86225676427bb5
Stanisław Halik <sthalik@misaki.pl>
2013-06-03 00:00:24 +0200

    Update coefficients

--- ftnoir_tracker_ht/ftnoir_tracker_ht.cpp
+++ ftnoir_tracker_ht/ftnoir_tracker_ht.cpp
@@ -109,14 +109,14 @@ static void load_settings(ht_config_t* config, Tracker* tracker)
 	config->pyrlk_pyramids = 3;
     config->pyrlk_win_size_w = config->pyrlk_win_size_h = 21;
     config->max_keypoints = 200;
-    config->keypoint_distance = 3.2;
+    config->keypoint_distance = 3;
     //config->force_width = 640;
     //config->force_height = 480;
     config->force_fps = iniFile.value("fps", 0).toInt();
     config->camera_index = iniFile.value("camera-index", -1).toInt();
     config->ransac_num_iters = 100;
-    config->ransac_max_reprojection_error = 3.8;
-    config->ransac_max_inlier_error = 3.8;
+    config->ransac_max_reprojection_error = 3.9;
+    config->ransac_max_inlier_error = 4;
     config->ransac_abs_max_mean_error = 8;
     config->ransac_max_mean_error = 3.4;
     config->debug = 0;
