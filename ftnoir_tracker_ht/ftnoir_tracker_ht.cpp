460713722f3668d88ea719c355cba148a28afeaf
Stanislaw Halik <sthalik@misaki.pl>
2013-07-21 23:19:28 +0200

    Slightly reduce max mean error (adjusted for distance from sensor)

--- ftnoir_tracker_ht/ftnoir_tracker_ht.cpp
+++ ftnoir_tracker_ht/ftnoir_tracker_ht.cpp
@@ -109,18 +109,18 @@ static void load_settings(ht_config_t* config, Tracker* tracker)
 	config->pyrlk_pyramids = 3;
     config->pyrlk_win_size_w = config->pyrlk_win_size_h = 21;
     config->max_keypoints = 200;
-    config->keypoint_distance = 4.75;
+    config->keypoint_distance = 4.8;
     //config->force_width = 640;
     //config->force_height = 480;
     config->force_fps = iniFile.value("fps", 0).toInt();
     config->camera_index = iniFile.value("camera-index", -1).toInt();
     config->ransac_num_iters = 100;
-    config->ransac_max_reprojection_error = 5.99;
+    config->ransac_max_reprojection_error = 6;
     config->ransac_max_inlier_error = 6;
     config->ransac_abs_max_mean_error = 15;
-    config->ransac_max_mean_error = 4.2;
+    config->ransac_max_mean_error = 4;
     config->debug = 0;
-    config->ransac_min_features = 0.87;
+    config->ransac_min_features = 0.85;
     int res = iniFile.value("resolution", 0).toInt();
     if (res < 0 || res >= (int)(sizeof(resolution_choices) / sizeof(resolution_tuple)))
 		res = 0;
