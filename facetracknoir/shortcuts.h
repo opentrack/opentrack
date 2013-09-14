26ccae712d899782ce606317f0fe6d34e869edce
Stanislaw Halik <sthalik@misaki.pl>
2013-09-14 17:59:21 +0200

    Move keyboard shortcuts to a new file
    Windows build could be interminently broken. Will
    resolve in following commits.

---
+++ facetracknoir/shortcuts.h
@@ -0,0 +1,89 @@
+#pragma once
+#include <QWidget>
+#include <QElapsedTimer>
+#include <QThread>
+#include "ui_ftnoir_keyboardshortcuts.h"
+
+class FaceTrackNoIR;
+
+class KeyboardShortcutDialog: public QWidget
+{
+    Q_OBJECT
+public:
+
+	explicit KeyboardShortcutDialog( FaceTrackNoIR *ftnoir, QWidget *parent=0, Qt::WindowFlags f=0 );
+    virtual ~KeyboardShortcutDialog();
+	void showEvent ( QShowEvent * event );
+
+private:
+	Ui::UICKeyboardShortcutDialog ui;
+	void loadSettings();
+	void save();
+
+	bool settingsDirty;
+	FaceTrackNoIR *mainApp;
+
+private slots:
+	void doOK();
+	void doCancel();
+};
+
+extern QList<QString> global_key_sequences;
+
+#if defined(__WIN32) || defined(_WIN32)
+extern QList<int> global_windows_key_sequences;
+#   undef DIRECTINPUT_VERSION
+#   define DIRECTINPUT_VERSION 0x0800
+#   include <windows.h>
+#   undef DIRECTINPUT_VERSION
+#   define DIRECTINPUT_VERSION 0x0800
+#   include <dinput.h>
+
+struct Key {
+    BYTE keycode;
+    bool shift;
+    bool ctrl;
+    bool alt;
+    bool ever_pressed;
+    QElapsedTimer timer;
+public:
+    Key() : keycode(0), shift(false), ctrl(false), alt(false), ever_pressed(false)
+    {
+    }
+};
+#else
+typedef unsigned char BYTE;
+struct Key { int foo; };
+#endif
+
+#if defined(__WIN32) || defined(_WIN32)
+class KeybindingWorkerImpl {
+private:
+    LPDIRECTINPUT8 din;
+    LPDIRECTINPUTDEVICE8 dinkeyboard;
+    Key kCenter;
+    FaceTrackNoIR& window;
+public:
+    volatile bool should_quit;
+    ~KeybindingWorkerImpl();
+    KeybindingWorkerImpl(FaceTrackNoIR& w, Key keyCenter);
+	void run();
+};
+#else
+class KeybindingWorkerImpl {
+public:
+    KeybindingWorkerImpl(FaceTrackNoIR& w, Key keyCenter);
+	void run() {}
+};
+#endif
+
+class KeybindingWorker : public QThread, public KeybindingWorkerImpl {
+	Q_OBJECT
+public:
+	KeybindingWorker(FaceTrackNoIR& w, Key keyCenter) : KeybindingWorkerImpl(w, keyCenter)
+	{
+	}
+	void run() {
+		KeybindingWorkerImpl::run();
+	}
+};
