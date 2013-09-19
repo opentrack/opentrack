109a6a10e23d95e8b8b7792ba3b8ebb738d21be4
Stanislaw Halik <sthalik@misaki.pl>
2013-09-19 19:39:50 +0200

    base: remove incorrect copyright/authorship info

--- ftnoir_tracker_base/ftnoir_tracker_types.h
+++ ftnoir_tracker_base/ftnoir_tracker_types.h
@@ -1,32 +1,4 @@
-/********************************************************************************
-* FaceTrackNoIR		This program is a private project of the some enthusiastic	*
-*					gamers from Holland, who don't like to pay much for			*
-*					head-tracking.												*
-*																				*
-* Copyright (C) 2010	Wim Vriend (Developing)									*
-*						Ron Hendriks (Researching and Testing)					*
-*																				*
-* Homepage																		*
-*																				*
-* This program is free software; you can redistribute it and/or modify it		*
-* under the terms of the GNU General Public License as published by the			*
-* Free Software Foundation; either version 3 of the License, or (at your		*
-* option) any later version.													*
-*																				*
-* This program is distributed in the hope that it will be useful, but			*
-* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY	*
-* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for	*
-* more details.																	*
-*																				*
-* You should have received a copy of the GNU General Public License along		*
-* with this program; if not, see <http://www.gnu.org/licenses/>.				*
-* This class implements a tracker-base											*
-*********************************************************************************/
-#ifndef FTNOIR_TRACKER_TYPES_H
-#define FTNOIR_TRACKER_TYPES_H
-
-
+#pragma once 
 enum Axis {
-    TX = 0, TY, TZ, Yaw, Pitch, Roll};
-
-#endif /*FTNOIR_TRACKER_TYPES_H*/
+    TX = 0, TY, TZ, Yaw, Pitch, Roll
+};
