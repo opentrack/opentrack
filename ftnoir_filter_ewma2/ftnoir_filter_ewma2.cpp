opentrack/opentrack

commit c4f6c12187ed423edc17501b0f6bda129d192bca

Fix ewma2 bitrot (by dbaarda)

ftnoir_filter_ewma2/ftnoir_filter_ewma2.cpp
@@ -123,7 +123,7 @@ void FTNoIR_Filter::FilterHeadPoseData(double *current_camera_position, double *
 //	if (newTarget) {
         for (int i=0;i<6;i++)
 		{
-            alpha[i]=1.0/(kMinSmoothing+((1.0-pow(norm_output_delta[i],kSmoothingScaleCurve))*smoothing_frames_range));
+            alpha[i]=1.0/(kMinSmoothing+((1.0-pow(norm_output_delta[i],kSmoothingScaleCurve))*(kMaxSmoothing - kMinSmoothing)));
 			smoothed_alpha[i]=(alpha_smoothing*alpha[i])+((1.0f-alpha_smoothing)*prev_alpha[i]);
 		}
 //	}

ftnoir_filter_ewma2/ftnoir_filter_ewma2.h
@@ -50,7 +50,6 @@ private:
     double newHeadPose;								// Structure with new headpose
 
 	bool	first_run;
-    double	smoothing_frames_range;
     double	alpha_smoothing;
     double	prev_alpha[6];
     double	alpha[6];

commit 214a8083c3a4e794c0d88b939606a263dc045563

ewma: fix filter lack of idempotence
Bug introduced by self in c4f6c12187ed423edc17501b0f6bda129d192bca

ftnoir_filter_ewma2/ftnoir_filter_ewma2.cpp
@@ -115,7 +115,7 @@ void FTNoIR_Filter::FilterHeadPoseData(const double *target_camera_position,
     //NB: larger alpha = *less* lag (opposite to what you'd expect)
     float largest_alpha=0.0f;
     for (int i=0;i<6;i++) {
-        largest_alpha=std::max<double>(largest_alpha, alpha[i]);
+        largest_alpha=std::min<double>(largest_alpha, alpha[i]);
     }
 
     // Calculate the new camera position.