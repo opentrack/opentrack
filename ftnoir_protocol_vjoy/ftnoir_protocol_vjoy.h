c1e08d60e51ce515273a6e5b9d31db5425025a58
Stanislaw Halik <sthalik@misaki.pl>
2013-10-15 13:22:35 +0200

    fix vjoy the hard way

--- ftnoir_protocol_vjoy/ftnoir_protocol_vjoy.h
+++ ftnoir_protocol_vjoy/ftnoir_protocol_vjoy.h
@@ -115,6 +115,15 @@ typedef struct _JOYSTICK_STATE
 
 #include <poppack.h>
 
-extern "C" BOOL __stdcall VJoy_Initialize(PCHAR name, PCHAR serial);
-extern "C" VOID __stdcall VJoy_Shutdown();
-extern "C" BOOL __stdcall VJoy_UpdateJoyState(int id, PJOYSTICK_STATE pJoyState);
+#undef EXTERN_C
+#if _MSC_VER
+#	define EXTERN_C
+#else
+#	define EXTERN_C extern "C"
+#endif
+#if _MSC_VER
+#	pragma comment(linker, "/implib:vjoy.def")
+#endif
+EXTERN_C BOOL __stdcall VJoy_Initialize(PCHAR name, PCHAR serial);
+EXTERN_C VOID __stdcall VJoy_Shutdown();
+EXTERN_C BOOL __stdcall VJoy_UpdateJoyState(int id, PJOYSTICK_STATE pJoyState);
