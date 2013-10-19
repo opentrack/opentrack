726875b0b40f8a43ae5ca763667dc707e84f39aa
mm0zct <the_cartographer@hotmail.com>
2013-10-19 18:36:19 +0100

    added todo and virtual function in tracker base class

--- ftnoir_tracker_base/ftnoir_tracker_base.h
+++ ftnoir_tracker_base/ftnoir_tracker_base.h
@@ -44,6 +44,7 @@ struct ITracker
     virtual ~ITracker() = 0;
     virtual void StartTracker( QFrame* frame ) = 0;
     virtual bool GiveHeadPoseData(double *data) = 0;
+	virtual void HotkeyCallback(unsigned int *) = 0;
 };
 
 inline ITracker::~ITracker() { }
