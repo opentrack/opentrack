ab19b6fe6d778b09e0b13d20491f636167e122e0
Stanislaw Halik <sthalik@misaki.pl>
2013-10-24 21:35:07 +0200

    add joystick tracker copyright info

--- ftnoir_tracker_joystick/ftnoir_tracker_joystick.h
+++ ftnoir_tracker_joystick/ftnoir_tracker_joystick.h
@@ -1,3 +1,9 @@
+/* Copyright (c) 2013 Stanislaw Halik <sthalik@misaki.pl>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ */
 #pragma once
 #include "ftnoir_tracker_base/ftnoir_tracker_base.h"
 #include "ui_ftnoir_tracker_joystick_controls.h"
