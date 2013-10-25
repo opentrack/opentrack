a2b8b3f7bd871c52164147cddc84ff68593e73e8
Stanislaw Halik <sthalik@misaki.pl>
2013-10-25 14:44:15 +0200

    Fix installer version

--- opentrack-version.h
+++ opentrack-version.h
@@ -1,3 +1,7 @@
 #ifndef OPENTRACK_VERSION
-#   define OPENTRACK_VERSION "Mourns-For-Trees"
+#   define OPENTRACK_VERSION @OPENTRACK_COMMIT_VERSION@
+#else
+#   ifndef OPENTRACK_VERSION
+#       define OPENTRACK_VERSION "Mourns-For-Trees"
+#   endif
 #endif
