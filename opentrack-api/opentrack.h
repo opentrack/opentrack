181875f3385391aed537c7e5fcff8fda199d39e6
Stanislaw Halik <sthalik@misaki.pl>
2013-10-30 17:54:51 +0100

    use version script
    Signed-off-by: Stanislaw Halik <sthalik@misaki.pl>

--- opentrack-api/opentrack.h
+++ opentrack-api/opentrack.h
@@ -25,12 +25,6 @@ typedef opentrack_opaque_tracker* opentrack_tracker;
 #   endif
 #endif
 
-#ifdef __GNUC__
-#   define OPENTRACK_VISIBLE __attribute__((visibility ("default")))
-#else
-#   define OPENTRACK_VISIBLE
-#endif
-
 /* for `opentrack_tracker_tick', individual headpose elts */
 #ifndef IN_OPENTRACK
 enum opentrack_dof {
@@ -44,20 +38,20 @@ enum opentrack_dof {
 };
 #endif
 
-opentrack OPENTRACK_VISIBLE opentrack_make_ctx(const char* dir);
-void OPENTRACK_VISIBLE opentrack_finalize_ctx(opentrack self);
+opentrack opentrack_make_ctx(const char* dir);
+void opentrack_finalize_ctx(opentrack self);
 
 /* no need to free the return value; invalid to modify it */
-const char** OPENTRACK_VISIBLE opentrack_enum_trackers(opentrack self);
+const char** opentrack_enum_trackers(opentrack self);
 
 /*
  * don't `opentrack_tracker_tick an unstarted tracker, it's invalid to do so
  * it's also invalid to start a finalized tracker
  */
-opentrack_tracker OPENTRACK_VISIBLE opentrack_make_tracker(opentrack ctx, const char* name);
-void OPENTRACK_VISIBLE opentrack_tracker_start(opentrack self, opentrack_tracker tracker);
-int OPENTRACK_VISIBLE opentrack_tracker_tick(opentrack_tracker tracker, double* headpose);
-void OPENTRACK_VISIBLE opentrack_finalize_tracker(opentrack_tracker tracker);
+opentrack_tracker opentrack_make_tracker(opentrack ctx, const char* name);
+void opentrack_tracker_start(opentrack self, opentrack_tracker tracker);
+int opentrack_tracker_tick(opentrack_tracker tracker, double* headpose);
+void opentrack_finalize_tracker(opentrack_tracker tracker);
 #ifdef __cplusplus
 }
 #endif
