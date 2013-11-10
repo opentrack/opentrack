3cfc21be2b2adec4dc55e17b316d8aea302af776
Stanislaw Halik <sthalik@misaki.pl>
2013-11-10 22:11:19 +0100

    hopefully fix qxt/qt eating letter keys
    Signed-off-by: Stanislaw Halik <sthalik@misaki.pl>

--- facetracknoir/facetracknoir.cpp
+++ facetracknoir/facetracknoir.cpp
@@ -758,7 +758,7 @@ void FaceTrackNoIR::bind_keyboard_shortcut(QxtGlobalShortcut& key, const QString
                 seq = "Alt+" + seq;
             if (iniFile.value(QString("Ctrl_%1").arg(label), false).toBool())
                 seq = "Ctrl+" + seq;
-            key.setShortcut(QKeySequence::fromString(seq, QKeySequence::NativeText));
+            key.setShortcut(QKeySequence::fromString(seq));
             key.setEnabled();
         }
     }
