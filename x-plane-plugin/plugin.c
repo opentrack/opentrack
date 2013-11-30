f413502750855ea1394e12650395697bcab01498
Stanislaw Halik <sthalik@misaki.pl>
2013-11-30 18:01:04 +0100

    fix x-plane crashing on exit
    Signed-off-by: Stanislaw Halik <sthalik@misaki.pl>

--- x-plane-plugin/plugin.c
+++ x-plane-plugin/plugin.c
@@ -129,7 +129,11 @@ PLUGIN_API int XPluginStart ( char * outName, char * outSignature, char * outDes
 
 PLUGIN_API void XPluginStop ( void ) {
     if (lck_posix)
+    {
         PortableLockedShm_free(lck_posix);
+        lck_posix = NULL;
+        shm_posix = NULL;
+    }
 }
 
 PLUGIN_API void XPluginEnable ( void ) {
