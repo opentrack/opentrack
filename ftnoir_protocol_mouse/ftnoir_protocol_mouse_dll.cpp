4a9c157caa350f0e333cba8444793ddae840bf12
Stanislaw Halik <sthalik@misaki.pl>
2014-01-01 10:33:12 +0100

    settings for mouse

--- ftnoir_protocol_mouse/ftnoir_protocol_mouse_dll.cpp
+++ ftnoir_protocol_mouse/ftnoir_protocol_mouse_dll.cpp
@@ -23,25 +23,7 @@
 *																				*
 ********************************************************************************/
 #include "ftnoir_protocol_mouse.h"
-#include <QDebug>
 #include "facetracknoir/global-settings.h"
-
-FTNoIR_ProtocolDll::FTNoIR_ProtocolDll() {
-}
-
-FTNoIR_ProtocolDll::~FTNoIR_ProtocolDll()
-{
-
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// Factory function that creates instances if the Protocol object.
-
-// Export both decorated and undecorated names.
-//   GetProtocolDll     - Undecorated name, which can be easily used with GetProcAddress
-//						Win32 API function.
-//   _GetProtocolDll@0  - Common name decoration for __stdcall functions in C language.
-//#pragma comment(linker, "/export:GetProtocolDll=_GetProtocolDll@0")
 
 extern "C" FTNOIR_PROTOCOL_BASE_EXPORT Metadata* CALLING_CONVENTION GetMetadata()
 {
