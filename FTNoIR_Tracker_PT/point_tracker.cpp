a91ddbaab2939d8fa145b693322cc09ba159c5d2
Stanislaw Halik <sthalik@misaki.pl>
2014-01-12 19:50:06 +0100

    Revert "fix translation estimation"
    This reverts commit 5cf25c6d0e524d21ea3eb9691f922b8c9b2fd73d.

--- FTNoIR_Tracker_PT/point_tracker.cpp
+++ FTNoIR_Tracker_PT/point_tracker.cpp
@@ -240,11 +240,9 @@ void PointTracker::POSIT(float fov, int w, int h)
     obj_points.push_back(point_model->M01);
     obj_points.push_back(point_model->M02);
 
-    for (int i = 0; i < 3; i++)
-    {
-        auto p2 = cv::Point(p[i][0] * w + w/2, p[i][1] * h + h/2);
-        img_points.push_back(p2);
-    }
+    img_points.push_back(p[0]);
+    img_points.push_back(p[1]);
+    img_points.push_back(p[2]);
 
     const float HT_PI = 3.1415926535;
 
