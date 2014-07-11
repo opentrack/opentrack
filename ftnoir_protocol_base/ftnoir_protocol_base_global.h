790cb945566e0575cbdd2962cc3189db26f0c0ee
Stanislaw Halik <sthalik@misaki.pl>
2014-07-11 11:31:42 +0200

    dedup export defs

--- ftnoir_protocol_base/ftnoir_protocol_base_global.h
+++ ftnoir_protocol_base/ftnoir_protocol_base_global.h
@@ -1,16 +1,4 @@
-#ifndef FTNOIR_PROTOCOL_BASE_GLOBAL_H
-#define FTNOIR_PROTOCOL_BASE_GLOBAL_H
+#pragma once
+#include "ftnoir_tracker_base/ftnoir_tracker_base.h"
 
-#include <QtGlobal>
-
-#ifndef OPENTRACK_MAIN
-# if !defined(_MSC_VER)
-#   define FTNOIR_PROTOCOL_BASE_EXPORT __attribute__ ((visibility ("default")))
-# else
-#   define FTNOIR_PROTOCOL_BASE_EXPORT Q_DECL_EXPORT
-#endif
-#else
-# define FTNOIR_PROTOCOL_BASE_EXPORT Q_DECL_IMPORT
-#endif
-
-#endif // FTNOIR_PROTOCOL_BASE_GLOBAL_H
+#define FTNOIR_PROTOCOL_BASE_EXPORT FTNOIR_TRACKER_BASE_EXPORT
