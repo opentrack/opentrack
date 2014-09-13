8a45fe928a7e73522759e2c1a8044d6cfce66186
Stanislaw Halik <sthalik@misaki.pl>
2014-09-13 13:15:57 +0200

    nix wrong attribution

--- ftnoir_protocol_wine/ftnoir_protocol_wine.h
+++ ftnoir_protocol_wine/ftnoir_protocol_wine.h
@@ -1,32 +1,4 @@
-/********************************************************************************
-* FaceTrackNoIR		This program is a private project of some enthusiastic		*
-*					gamers from Holland, who don't like to pay much for			*
-*					head-tracking.												*
-*																				*
-* Copyright (C) 2010	Wim Vriend (Developing)									*
-*						Ron Hendriks (Researching and Testing)					*
-*																				*
-* Homepage																		*
-*																				*
-* This program is free software; you can redistribute it and/or modify it		*
-* under the terms of the GNU General Public License as published by the			*
-* Free Software Foundation; either version 3 of the License, or (at your		*
-* option) any later version.													*
-*																				*
-* This program is distributed in the hope that it will be useful, but			*
-* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY	*
-* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for	*
-* more details.																	*
-*																				*
-* You should have received a copy of the GNU General Public License along		*
-* with this program; if not, see <http://www.gnu.org/licenses/>.				*
-*																				*
-* FTServer		FTServer is the Class, that communicates headpose-data			*
-*				to games, using the FreeTrackClient.dll.		         		*
-********************************************************************************/
 #pragma once
-#ifndef INCLUDED_FTSERVER_H
-#define INCLUDED_FTSERVER_H
 
 #include "ftnoir_protocol_base/ftnoir_protocol_base.h"
 #include "ftnoir_protocol_ft/fttypes.h"
@@ -92,7 +64,3 @@ public:
 
     void getIcon(QIcon *icon) { *icon = QIcon(":/images/wine.png"); }
 };
-
-
-#endif//INCLUDED_FTSERVER_H
-//END
