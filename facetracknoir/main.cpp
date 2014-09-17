03e4d58bbbbfb5d841354a34a2e2dc317b189deb
Stanislaw Halik <sthalik@misaki.pl>
2014-09-17 08:07:09 +0200

    initialize COM on _WIN32 as per Win32 API
    For API correctness only. It worked without issues since ever.

--- facetracknoir/main.cpp
+++ facetracknoir/main.cpp
@@ -2,8 +2,16 @@
 #include <QApplication>
 #include <memory>
 
+#ifdef _WIN32
+#   include <objbase.h>
+#endif
+
 int main(int argc, char** argv)
 {
+#ifdef _WIN32
+    CoInitializeEx(NULL, COINIT_MULTITHREADED);
+#endif
+    
     QApplication::setAttribute(Qt::AA_X11InitThreads, true);
     QApplication app(argc, argv);
     auto w = std::make_shared<FaceTrackNoIR>();
