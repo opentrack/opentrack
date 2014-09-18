20b2685c74ae615420517e93eac0d2c06f6b93cc
Stanislaw Halik <sthalik@misaki.pl>
2014-09-18 08:48:08 +0200

    ft: use C-style comments

--- ftnoir_protocol_ft/fttypes.h
+++ ftnoir_protocol_ft/fttypes.h
@@ -24,26 +24,26 @@
 #define FT_MM_DATA "FT_SharedMem"
 #define FREETRACK_MUTEX "FT_Mutext"
 
-// only 6 headpose floats and the data id are filled -sh
+/* only 6 headpose floats and the data id are filled -sh */
 typedef struct __FTData {
     int DataID;
     int CamWidth;
     int CamHeight;
-    // virtual pose
-    float Yaw;   // positive yaw to the left
-    float Pitch; // positive pitch up
-    float Roll;  // positive roll to the left
+    /* virtual pose */
+    float Yaw;   /* positive yaw to the left */
+    float Pitch; /* positive pitch up */
+    float Roll;  /* positive roll to the left */
     float X;
     float Y;
     float Z;
-    // raw pose with no smoothing, sensitivity, response curve etc. 
+    /* raw pose with no smoothing, sensitivity, response curve etc. */
     float RawYaw;
     float RawPitch;
     float RawRoll;
     float RawX;
     float RawY;
     float RawZ;
-    // raw points, sorted by Y, origin top left corner
+    /* raw points, sorted by Y, origin top left corner */
     float X1;
     float Y1;
     float X2;
