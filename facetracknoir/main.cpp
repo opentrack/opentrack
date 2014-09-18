eb09eda1ca927b0a3fbbab1ba95acc86b33efd5f
Stanislaw Halik <sthalik@misaki.pl>
2014-09-18 04:20:20 +0200

    Revert "initialize COM on _WIN32 as per Win32 API"
    This reverts commit 03e4d58bbbbfb5d841354a34a2e2dc317b189deb.
    
    Issue: #53
    
    Ironically breaks Windows open/save dialogs

--- facetracknoir/main.cpp
+++ facetracknoir/main.cpp
@@ -2,16 +2,8 @@
 #include <QApplication>
 #include <memory>
 
-#ifdef _WIN32
-#   include <objbase.h>
-#endif
-
 int main(int argc, char** argv)
 {
-#ifdef _WIN32
-    CoInitializeEx(NULL, COINIT_MULTITHREADED);
-#endif
-    
     QApplication::setAttribute(Qt::AA_X11InitThreads, true);
     QApplication app(argc, argv);
     auto w = std::make_shared<FaceTrackNoIR>();
