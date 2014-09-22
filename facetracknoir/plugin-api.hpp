45d89cfd526bf7fa3f7865f2e78f10cd9a85a2f6
Stanislaw Halik <sthalik@misaki.pl>
2014-09-22 15:20:15 +0200

    fix Linux build

--- facetracknoir/plugin-api.hpp
+++ facetracknoir/plugin-api.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <QtGlobal>
-#include <QFrame>
+#include "facetracknoir/export.hpp"
 
 #if defined(_WIN32)
 #   define CALLING_CONVENTION __stdcall
@@ -13,75 +12,6 @@ enum Axis {
     TX = 0, TY, TZ, Yaw, Pitch, Roll
 };
 
-struct Metadata
-{
-    Metadata() {}
-    virtual ~Metadata() {}
-
-    virtual void getFullName(QString *strToBeFilled) = 0;
-    virtual void getShortName(QString *strToBeFilled) = 0;
-    virtual void getDescription(QString *strToBeFilled) = 0;
-    virtual void getIcon(QIcon *icon) = 0;
-};
-
-struct IFilter
-{
-    virtual ~IFilter() = 0;
-    virtual void FilterHeadPoseData(const double *target_camera_position, double *new_camera_position) = 0;
-    virtual void reset() = 0;
-};
-
-inline IFilter::~IFilter() {}
-
-struct IFilterDialog
-{
-    virtual ~IFilterDialog() {}
-    virtual void registerFilter(IFilter* tracker) = 0;
-    virtual void unregisterFilter() = 0;
-};
-
-struct IProtocol
-{
-    virtual ~IProtocol() = 0;
-    virtual bool checkServerInstallationOK() = 0;
-    virtual void sendHeadposeToGame( const double* headpose ) = 0;
-    virtual QString getGameName() = 0;
-};
-
-inline IProtocol::~IProtocol() {}
-
-struct IProtocolDialog
-{
-    virtual ~IProtocolDialog() {}
-    virtual void registerProtocol(IProtocol *protocol) = 0;
-    virtual void unRegisterProtocol() = 0;
-};
-
-struct ITracker
-{
-    virtual ~ITracker() = 0;
-    virtual void StartTracker( QFrame* frame ) = 0;
-    virtual void GetHeadPoseData(double *data) = 0;
-    virtual int preferredHz() { return 200; }
-};
-
-inline ITracker::~ITracker() {}
-
-struct ITrackerDialog
-{
-    virtual ~ITrackerDialog() {}
-    virtual void registerTracker(ITracker *tracker) = 0;
-    virtual void unRegisterTracker() = 0;
-};
-
-#ifndef OPENTRACK_EXPORT
-#   ifdef IN_OPENTRACK
-#    if !defined(_MSC_VER)
-#      define OPENTRACK_EXPORT __attribute__ ((visibility ("default"))) Q_DECL_EXPORT
-#    else
-#     error "MSVC support removed"
-#   endif
-#   else
-#       error "Use only for exporting dynamic modules"
-#   endif
+#ifndef OPENTRACK_CROSS_ONLY
+#   include "facetracknoir/plugin-qt-api.hpp"
 #endif
---
+++ facetracknoir/plugin-qt-api.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <QString>
+#include <QFrame>
+
+struct Metadata
+{
+    Metadata() {}
+    virtual ~Metadata() {}
+
+    virtual void getFullName(QString *strToBeFilled) = 0;
+    virtual void getShortName(QString *strToBeFilled) = 0;
+    virtual void getDescription(QString *strToBeFilled) = 0;
+    virtual void getIcon(QIcon *icon) = 0;
+};
+
+struct IFilter
+{
+    virtual ~IFilter() = 0;
+    virtual void FilterHeadPoseData(const double *target_camera_position, double *new_camera_position) = 0;
+    virtual void reset() = 0;
+};
+inline IFilter::~IFilter() {}
+
+struct IFilterDialog
+{
+    virtual ~IFilterDialog() = 0;
+    virtual void registerFilter(IFilter* tracker) = 0;
+    virtual void unregisterFilter() = 0;
+};
+inline IFilterDialog::~IFilterDialog() {}
+
+struct IProtocol
+{
+    virtual ~IProtocol() = 0;
+    virtual bool checkServerInstallationOK() = 0;
+    virtual void sendHeadposeToGame( const double* headpose ) = 0;
+    virtual QString getGameName() = 0;
+};
+inline IProtocol::~IProtocol() {}
+
+struct IProtocolDialog
+{
+    virtual ~IProtocolDialog() = 0;
+    virtual void registerProtocol(IProtocol *protocol) = 0;
+    virtual void unRegisterProtocol() = 0;
+};
+inline IProtocolDialog::~IProtocolDialog() {}
+
+struct ITracker
+{
+    virtual ~ITracker() = 0;
+    virtual void StartTracker( QFrame* frame ) = 0;
+    virtual void GetHeadPoseData(double *data) = 0;
+    virtual int preferredHz() { return 200; }
+};
+inline ITracker::~ITracker() {}
+
+struct ITrackerDialog
+{
+    virtual ~ITrackerDialog() = 0;
+    virtual void registerTracker(ITracker *tracker) = 0;
+    virtual void unRegisterTracker() = 0;
+};
+inline ITrackerDialog::~ITrackerDialog() {}
