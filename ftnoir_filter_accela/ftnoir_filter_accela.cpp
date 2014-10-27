58ab6bccdadcf8bafd03ad3861c6f2058557cd6f
Stanislaw Halik <sthalik@misaki.pl>
2014-10-27 11:21:52 +0100

    allow for even more faster response in slider

--- ftnoir_filter_accela/ftnoir_filter_accela.cpp
+++ ftnoir_filter_accela/ftnoir_filter_accela.cpp
@@ -52,7 +52,7 @@ void FTNoIR_Filter::filter(const double* input, double *output)
     for (int i = 0; i < 6; i++)
     {
         const double vec = input[i] - last_output[i];
-        double datum = Hz * 24;
+        double datum = Hz * 64;
 
         if (i >= 3)
         {
