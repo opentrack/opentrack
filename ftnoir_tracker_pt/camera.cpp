0148f7dee879cd19a1518b483cf0d2ebc5ec1c5c
Stanislaw Halik <sthalik@misaki.pl>
2014-11-23 15:10:59 +0100

    pt: nix old camera code

--- ftnoir_tracker_pt/camera.cpp
+++ ftnoir_tracker_pt/camera.cpp
@@ -143,8 +143,6 @@ bool Camera::get_frame(float dt, cv::Mat* frame)
 	return new_frame;
 }
 
-// ----------------------------------------------------------------------------
-#ifdef OPENTRACK_API
 void CVCamera::start()
 {
     cap = new VideoCapture(desired_index);
@@ -214,91 +212,3 @@ void CVCamera::_set_device_index()
     }
     cap = new VideoCapture(desired_index);
 }
-
-#else
-// ----------------------------------------------------------------------------
-VICamera::VICamera() : frame_buffer(NULL)
-{
-	VI.listDevices();
-}
-
-void VICamera::start()
-{
-	if (desired_index >= 0)
-	{	
-		if (cam_desired.res_x == 0 || cam_desired.res_y == 0)
-			VI.setupDevice(desired_index);
-		else
-			VI.setupDevice(desired_index, cam_desired.res_x, cam_desired.res_y);
-
-		active = true;
-		active_index = desired_index;
-
-		cam_info.res_x = VI.getWidth(active_index);
-		cam_info.res_y = VI.getHeight(active_index);
-		new_frame = cv::Mat(cam_info.res_y, cam_info.res_x, CV_8UC3);
-		// If matrix is not continuous we have to copy manually via frame_buffer
-		if (!new_frame.isContinuous()) {
-			unsigned int size = VI.getSize(active_index);
-			frame_buffer = new unsigned char[size];
-		}
-	}
-}
-
-void VICamera::stop()
-{
-	if (active)
-	{
-		VI.stopDevice(active_index);
-	}
-	if (frame_buffer)
-	{
-		delete[] frame_buffer;
-		frame_buffer = NULL;
-	}
-	active = false;
-}
-
-bool VICamera::_get_frame(Mat* frame)
-{
-	if (active && VI.isFrameNew(active_index))
-	{
-		if (new_frame.isContinuous())
-		{
-			VI.getPixels(active_index, new_frame.data, false, true);
-		}
-		else
-		{
-			// If matrix is not continuous we have to copy manually via frame_buffer
-			VI.getPixels(active_index, frame_buffer, false, true);
-			new_frame = cv::Mat(cam_info.res_y, cam_info.res_x, CV_8UC3, frame_buffer).clone();
-		}
-		*frame = new_frame;
-		return true;
-	}
-	return false;
-}
-
-void VICamera::_set_device_index()
-{
-	if (active) restart();
-}
-
-void VICamera::_set_f()
-{
-	cam_info.f = cam_desired.f;
-}
-
-void VICamera::_set_fps()
-{
-	bool was_active = active;
-	if (active) stop();
-	VI.setIdealFramerate(desired_index, cam_desired.fps);
-	if (was_active) start();
-}
-
-void VICamera::_set_res()
-{
-	if (active) restart();
-}
-#endif
