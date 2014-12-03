a692aff863ee03f201313168634a5ffe19890d83
Stanislaw Halik <sthalik@misaki.pl>
2014-12-03 18:39:47 +0100

    remove alphanumeric keybindings making list too long

--- opentrack/global-shortcuts.cpp
+++ opentrack/global-shortcuts.cpp
@@ -14,32 +14,6 @@ extern QList<int> global_windows_key_sequences;
 QList<int> global_windows_key_sequences =
     QList<int>()
     << 0
-    << DIK_A
-    << DIK_B
-    << DIK_C
-    << DIK_D
-    << DIK_E
-    << DIK_F
-    << DIK_G
-    << DIK_H
-    << DIK_I
-    << DIK_J
-    << DIK_K
-    << DIK_L
-    << DIK_M
-    << DIK_N
-    << DIK_O
-    << DIK_P
-    << DIK_Q
-    << DIK_R
-    << DIK_S
-    << DIK_T
-    << DIK_U
-    << DIK_V
-    << DIK_W
-    << DIK_X
-    << DIK_Y
-    << DIK_Z
     << DIK_F1
     << DIK_F2
     << DIK_F3
@@ -52,16 +26,6 @@ QList<int> global_windows_key_sequences =
     << DIK_F10
     << DIK_F11
     << DIK_F12
-    << DIK_1
-    << DIK_2
-    << DIK_3
-    << DIK_4
-    << DIK_5
-    << DIK_6
-    << DIK_7
-    << DIK_8
-    << DIK_9
-    << DIK_0
     << DIK_LEFT
     << DIK_RIGHT
     << DIK_UP
@@ -78,32 +42,6 @@ QList<int> global_windows_key_sequences =
 QList<QString> global_key_sequences =
     QList<QString>()
     << ""
-    << "A"
-    << "B"
-    << "C"
-    << "D"
-    << "E"
-    << "F"
-    << "G"
-    << "H"
-    << "I"
-    << "J"
-    << "K"
-    << "L"
-    << "M"
-    << "N"
-    << "O"
-    << "P"
-    << "Q"
-    << "R"
-    << "S"
-    << "T"
-    << "U"
-    << "V"
-    << "W"
-    << "X"
-    << "Y"
-    << "Z"
     << "F1"
     << "F2"
     << "F3"
@@ -116,16 +54,6 @@ QList<QString> global_key_sequences =
     << "F10"
     << "F11"
     << "F12"
-    << "1"
-    << "2"
-    << "3"
-    << "4"
-    << "5"
-    << "6"
-    << "7"
-    << "8"
-    << "9"
-    << "0"
     << "Left"
     << "Right"
     << "Up"
@@ -134,9 +62,7 @@ QList<QString> global_key_sequences =
     << "PgDown"
     << "Home"
     << "End"
-    << "Backspace"
     << "Del"
-    << "Enter"
 ;
 
 
