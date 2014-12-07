f22c5bfb6e1da213dba90d12d205fd136ac28245
Stanislaw Halik <sthalik@misaki.pl>
2014-12-07 15:10:59 +0100

    accela: adjust constants for faster response
    Tested-by: @KyokushinPL

--- ftnoir_filter_accela/ftnoir_filter_accela.h
+++ ftnoir_filter_accela/ftnoir_filter_accela.h
@@ -33,10 +33,10 @@ private:
     Timer t;
     static double f(double vec, double thres);
     
-    static constexpr double high_thres_c = 3;
-    static constexpr double high_thres_out = 350;
+    static constexpr double high_thres_c = 4;
+    static constexpr double high_thres_out = 500;
     
-    static constexpr double low_thres_mult = 50;
+    static constexpr double low_thres_mult = 100;
 };
 
 class FilterControls: public IFilterDialog
