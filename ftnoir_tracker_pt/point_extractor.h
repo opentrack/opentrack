20278ad8011e1edc5fc7afeef139625e54dbb21d
DaMichel <mw.pub@welter-4d.de>
2015-02-14 10:06:30 +0100

    pointtracker: removed hysteresis stuff. Implemented mean-shift filtering. Temporal noise is going to be taken care of by respective filters

--- ftnoir_tracker_pt/point_extractor.h
+++ ftnoir_tracker_pt/point_extractor.h
@@ -21,7 +21,7 @@ public:
     // extracts points from frame and draws some processing info into frame, if draw_output is set
     // dt: time since last call in seconds
     // WARNING: returned reference is valid as long as object
-    std::vector<cv::Vec2f> extract_points(cv::Mat &frame);
+    const std::vector<cv::Vec2f>& extract_points(cv::Mat &frame);
     const std::vector<cv::Vec2f>& get_points() { return points; }
     PointExtractor();
     
