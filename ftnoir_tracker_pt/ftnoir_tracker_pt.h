b9d5445341084c3842a2a9380dcd608a8abfc7e0
Stanislaw Halik <sthalik@misaki.pl>
2015-02-17 06:25:44 +0100

    pt: use volatile across threads

--- ftnoir_tracker_pt/ftnoir_tracker_pt.h
+++ ftnoir_tracker_pt/ftnoir_tracker_pt.h
@@ -72,7 +72,7 @@ private:
     settings s;
     Timer time;
     
-    bool success;
+    volatile bool success;
 
     static constexpr double rad2deg = 180.0/3.14159265;
     static constexpr double deg2rad = 3.14159265/180.0;
