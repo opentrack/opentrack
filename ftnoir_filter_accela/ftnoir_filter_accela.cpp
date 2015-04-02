ec3c1b6de6be845a3fe259c24d6fd075d985191c
Stanislaw Halik <sthalik@misaki.pl>
2015-04-02 13:23:12 +0200

    accela: update copyright

--- ftnoir_filter_accela/ftnoir_filter_accela.cpp
+++ ftnoir_filter_accela/ftnoir_filter_accela.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013 Stanislaw Halik
+/* Copyright (c) 2012-2015 Stanislaw Halik
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
