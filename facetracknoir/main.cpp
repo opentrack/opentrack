61c16798ad9b59d7507abccfc95ec2a2eac729fd
Fabian Wickborn <fabian@wickborn.net>
2015-05-20 15:02:47 +0200

    Added command line option for automatic starts

--- facetracknoir/main.cpp
+++ facetracknoir/main.cpp
@@ -1,5 +1,6 @@
 #include "ui.h"
 #include <QApplication>
+#include <QCommandLineParser>
 #include <QStyleFactory>
 #include <QStringList>
 #include <memory>
@@ -28,8 +29,21 @@ int main(int argc, char** argv)
     QApplication::setAttribute(Qt::AA_X11InitThreads, true);
     QApplication app(argc, argv);
 
+	QCommandLineParser p;
+	p.setApplicationDescription("opentrack - Head tracking software for MS Windows, Linux, and Apple OSX");
+	p.addHelpOption();
+	QCommandLineOption autostartOption(QStringList() << "a" << "autostart", "Load <profile> and start tracking", "profile");
+	p.addOption(autostartOption);
+	p.process(app);
+
     MainWindow w;
 
+	QString profile = p.value(autostartOption);
+	if (! profile.isEmpty() ) 
+	{
+		w.open_and_run(profile);
+	}
+		
     w.show();
     app.exec();
 
