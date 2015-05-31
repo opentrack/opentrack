04e8ee2b5a24d7ec3161d1abe834d6b90e829691
Stanislaw Halik <sthalik@misaki.pl>
2015-05-31 11:58:27 +0200

    main: switch rotation order for centering
    This helps some PT clip model users, but can possibly break inertial
    device centering, so needs further testing.

--- opentrack/tracker.cpp
+++ opentrack/tracker.cpp
@@ -130,7 +130,7 @@ void Tracker::logic()
     {
         double tmp[3] = { t(0, 0) - t_b[0], t(1, 0) - t_b[1], t(2, 0) - t_b[2] };
         t_compensate(cam, tmp, tmp, false);
-        const rmat m_ = r * r_b.t();
+        const rmat m_ = r_b.t() * r;
         const dmat<3, 1> euler = rmat::rmat_to_euler(m_);
         for (int i = 0; i < 3; i++)
         {
