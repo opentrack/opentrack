71b9202dbd592996478cb3ab67a84c23fd98368e
Stanislaw Halik <sthalik@misaki.pl>
2015-07-07 07:45:35 +0200

    opentrack-api: add copyright notices
    Code without a license defaults to having all rights reserved.

--- opentrack/plugin-support.hpp
+++ opentrack/plugin-support.hpp
@@ -1,11 +1,11 @@
-#pragma once
-
 /* Copyright (c) 2015 Stanislaw Halik
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  */
+
+#pragma once
 
 #include "plugin-api.hpp"
 #include "options.hpp"
--- opentrack/simple-mat.hpp
+++ opentrack/simple-mat.hpp
@@ -1,3 +1,11 @@
+/* Copyright (c) 2014-2015, Stanislaw Halik <sthalik@misaki.pl>
+
+ * Permission to use, copy, modify, and/or distribute this
+ * software for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice and this permission
+ * notice appear in all copies.
+ */
+
 #pragma once
 #include <algorithm>
 #include <initializer_list>
