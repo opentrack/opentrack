8cc9e744da96d9bb5e84b41f3052f69074a05438
Stanislaw Halik <sthalik@misaki.pl>
2015-07-20 04:56:53 +0200

    mouse: replace copyright
    The mouse tracker was entirely rewritten since.

--- ftnoir_protocol_mouse/ftnoir_protocol_mouse.cpp
+++ ftnoir_protocol_mouse/ftnoir_protocol_mouse.cpp
@@ -1,31 +1,9 @@
-/********************************************************************************
-* FaceTrackNoIR		This program is a private project of the some enthusiastic	*
-*					gamers from Holland, who don't like to pay much for			*
-*					head-tracking.												*
-*																				*
-* Copyright (C) 2010-2011	Wim Vriend (Developing)								*
-*							Ron Hendriks (Researching and Testing)				*
-*																				*
-* Homepage																		*
-*																				*
-* This program is free software; you can redistribute it and/or modify it		*
-* under the terms of the GNU General Public License as published by the			*
-* Free Software Foundation; either version 3 of the License, or (at your		*
-* option) any later version.													*
-*																				*
-* This program is distributed in the hope that it will be useful, but			*
-* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY	*
-* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for	*
-* more details.																	*
-*																				*
-* You should have received a copy of the GNU General Public License along		*
-* with this program; if not, see <http://www.gnu.org/licenses/>.				*
-*																				*
-* FTNoIR_Protocol_Mouse	The Class, that communicates headpose-data by			*
-*						generating Mouse commands.								*
-*						Many games (like FPS's) support Mouse-look features,	*
-*						but no face-tracking.									*
-********************************************************************************/
+/* Copyright (c) 2015 Stanislaw Halik <sthalik@misaki.pl>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ */
 #include "ftnoir_protocol_mouse.h"
 #include "opentrack/plugin-api.hpp"
 
