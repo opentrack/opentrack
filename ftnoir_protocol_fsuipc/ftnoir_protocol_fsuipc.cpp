96841349464fa38e6d0d5f79d66d4012b6ab315c
Stanislaw Halik <sthalik@misaki.pl>
2015-08-13 10:06:12 +0200

    fsuipc: add comment

--- ftnoir_protocol_fsuipc/ftnoir_protocol_fsuipc.cpp
+++ ftnoir_protocol_fsuipc/ftnoir_protocol_fsuipc.cpp
@@ -126,6 +126,9 @@ void FTNoIR_Protocol::pose(const double *headpose ) {
 			//
 			FSUIPC_Process(&result);
 			if (result == FSUIPC_ERR_SENDMSG) {
+                // FSUIPC checks for already open connections and returns FSUIPC_ERR_OPEN in that case
+                // the connection scope is global for the process. this is why above code doesn't
+                // leak resources or have logic errors. see: http://www.purebasic.fr/english/viewtopic.php?t=31112
 				FSUIPC_Close();							//timeout (1 second) so assume FS closed
 			}
 		}
