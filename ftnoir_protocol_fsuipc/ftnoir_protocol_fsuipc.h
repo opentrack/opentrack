64e188cf40afce73d8307b54053eefe9742fa966
Stanislaw Halik <sthalik@misaki.pl>
2015-10-11 06:08:13 +0200

    facetracknoir update license
    @V4Friend agreed to change license to non-copyleft.
    
    I thank him for this.

--- ftnoir_protocol_fsuipc/ftnoir_protocol_fsuipc.h
+++ ftnoir_protocol_fsuipc/ftnoir_protocol_fsuipc.h
@@ -1,29 +1,13 @@
-/********************************************************************************
-* FaceTrackNoIR         This program is a private project of some enthusiastic          *
-*                                       gamers from Holland, who don't like to pay much for                     *
-*                                       head-tracking.                                                                                          *
-*                                                                                                                                                               *
-* Copyright (C) 2010-2011       Wim Vriend (Developing)                                                         *
-*                                                       Ron Hendriks (Researching and Testing)                          *
-*                                                                                                                                                               *
-* Homepage                                                                                                                                              *
-*                                                                                                                                                               *
-* This program is free software; you can redistribute it and/or modify it               *
-* under the terms of the GNU General Public License as published by the                 *
-* Free Software Foundation; either version 3 of the License, or (at your                *
-* option) any later version.                                                                                                    *
-*                                                                                                                                                               *
-* This program is distributed in the hope that it will be useful, but                   *
-* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY    *
-* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for   *
-* more details.                                                                                                                                 *
-*                                                                                                                                                               *
-* You should have received a copy of the GNU General Public License along               *
-* with this program; if not, see <http://www.gnu.org/licenses/>.                                *
-*                                                                                                                                                               *
-* FSUIPCServer          FSUIPCServer is the Class, that communicates headpose-data      *
-*                                       to games, using the FSUIPC.dll.                                                 *
-********************************************************************************/
+/* Homepage         http://facetracknoir.sourceforge.net/home/default.htm        *
+ *                                                                               *
+ * ISC License (ISC)                                                             *
+ *                                                                               *
+ * Copyright (c) 2015, Wim Vriend                                                *
+ *                                                                               *
+ * Permission to use, copy, modify, and/or distribute this software for any      *
+ * purpose with or without fee is hereby granted, provided that the above        *
+ * copyright notice and this permission notice appear in all copies.             *
+ */
 #pragma once
 #ifndef INCLUDED_FSUIPCSERVER_H
 #define INCLUDED_FSUIPCSERVER_H
--- ftnoir_protocol_sc/ftnoir_protocol_sc.h
+++ ftnoir_protocol_sc/ftnoir_protocol_sc.h
@@ -1,31 +1,14 @@
-/********************************************************************************
-* FaceTrackNoIR         This program is a private project of some enthusiastic          *
-*                                       gamers from Holland, who don't like to pay much for                     *
-*                                       head-tracking.                                                                                          *
-*                                                                                                                                                               *
-* Copyright (C) 2010-2011       Wim Vriend (Developing)                                                         *
-*                                                       Ron Hendriks (Researching and Testing)                          *
-*                                                                                                                                                               *
-* Homepage                                                                                                                                              *
-*                                                                                                                                                               *
-* This program is free software; you can redistribute it and/or modify it               *
-* under the terms of the GNU General Public License as published by the                 *
-* Free Software Foundation; either version 3 of the License, or (at your                *
-* option) any later version.                                                                                                    *
-*                                                                                                                                                               *
-* This program is distributed in the hope that it will be useful, but                   *
-* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY    *
-* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for   *
-* more details.                                                                                                                                 *
-*                                                                                                                                                               *
-* You should have received a copy of the GNU General Public License along               *
-* with this program; if not, see <http://www.gnu.org/licenses/>.                                *
-*                                                                                                                                                               *
-* SCServer              SCServer is the Class, that communicates headpose-data                  *
-*                               to games, using the SimConnect.dll.                                                     *
-*                               SimConnect.dll is a so called 'side-by-side' assembly, so it    *
-*                               must be treated as such...                                                                              *
-********************************************************************************/
+/* Homepage         http://facetracknoir.sourceforge.net/home/default.htm        *
+ *                                                                               *
+ * ISC License (ISC)                                                             *
+ *                                                                               *
+ * Copyright (c) 2015, Wim Vriend                                                *
+ * Copyright (c) 2014, Stanislaw Halik                                           *
+ *                                                                               *
+ * Permission to use, copy, modify, and/or distribute this software for any      *
+ * purpose with or without fee is hereby granted, provided that the above        *
+ * copyright notice and this permission notice appear in all copies.             *
+ */
 #pragma once
 #include "opentrack/plugin-api.hpp"
 
