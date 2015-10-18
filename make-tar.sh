14d27dd0998b18a2eb13d30c8680701a9b619a08
Stanislaw Halik <sthalik@misaki.pl>
2015-10-18 09:38:10 +0200

    cmake: fix dropbox share invocation

--- make-tar.sh
+++ make-tar.sh
@@ -14,10 +14,9 @@ then
     case "$USER,$(uname -s)" in
         # for the script see https://github.com/andreafabrizi/Dropbox-Uploader
         sthalik,CYGWIN_*)
-            set -x
             dropbox_uploader.sh -p upload "$filename" /
-            l="$(dropbox_uploader.sh -q share "/$filename")"
-            set +x
+            bn="$(basename -- "$filename")"
+            l="$(dropbox_uploader.sh -q share /"$bn")"
             test -n "$l" && echo -n "$l" | putclip
             echo $l
             echo -ne '\a' ;;
