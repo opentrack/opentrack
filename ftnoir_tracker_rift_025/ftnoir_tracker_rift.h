ee5a2b5cd1e93a2133697445aa68f40539c902e2
Stanislaw Halik <sthalik@misaki.pl>
2015-10-28 13:45:21 +0100

    buffer flush

---
+++ ftnoir_tracker_rift_025/ftnoir_tracker_rift.h
@@ -0,0 +1,60 @@
+#pragma once
+#include "ui_ftnoir_rift_clientcontrols.h"
+#include <QMessageBox>
+#include <QWaitCondition>
+#include <cmath>
+#include "opentrack/plugin-api.hpp"
+#include "OVR.h"
+#include <memory>
+#include "opentrack/options.hpp"
+using namespace options;
+
+struct settings : opts {
+    value<bool> useYawSpring;
+    value<double> constant_drift, persistence, deadzone;
+    settings() :
+        opts("Rift"),
+        useYawSpring(b, "yaw-spring", false),
+        constant_drift(b, "constant-drift", 0.000005),
+        persistence(b, "persistence", 0.99999),
+        deadzone(b, "deadzone", 0.02)
+    {}
+};
+
+class Rift_Tracker : public ITracker
+{
+public:
+    Rift_Tracker();
+    ~Rift_Tracker() override;
+    void start_tracker(QFrame *) override;
+    void data(double *data) override;
+private:
+    double old_yaw;
+    ovrHmd hmd;
+    settings s;
+};
+
+class TrackerControls: public ITrackerDialog
+{
+    Q_OBJECT
+public:
+    TrackerControls();
+
+    void register_tracker(ITracker *) {}
+    void unregister_tracker() {}
+
+private:
+    Ui::UIRiftControls ui;
+    settings s;
+private slots:
+    void doOK();
+    void doCancel();
+};
+
+class FTNoIR_TrackerDll : public Metadata
+{
+public:
+    QString name() { return QString("Oculus Rift -- HMD"); }
+    QIcon icon() { return QIcon(":/images/rift_tiny.png"); }
+};
+
---
+++ ftnoir_tracker_rift_025/ftnoir_tracker_rift_dialog.cpp
@@ -0,0 +1,26 @@
+#include "ftnoir_tracker_rift.h"
+#include "opentrack/plugin-api.hpp"
+
+TrackerControls::TrackerControls()
+{
+    ui.setupUi( this );
+
+    connect(ui.buttonBox, SIGNAL(accepted()), this, SLOT(doOK()));
+    connect(ui.buttonBox, SIGNAL(rejected()), this, SLOT(doCancel()));
+
+    tie_setting(s.constant_drift, ui.constantDrift);
+    tie_setting(s.deadzone, ui.deadzone);
+    tie_setting(s.persistence, ui.persistence);
+    tie_setting(s.useYawSpring, ui.yawSpring);
+}
+
+void TrackerControls::doOK() {
+    s.b->save();
+	this->close();
+}
+
+void TrackerControls::doCancel() {
+    s.b->reload();
+    close();
+}
+
