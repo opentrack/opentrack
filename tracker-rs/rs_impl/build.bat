763da906a83b80eadb736008dbb558b809552505
Xavier Hallade <xavier.hallade@intel.com>
2015-11-19 14:34:26 +0100

    tracker-rs: updated RealSense SDK version to R5

--- tracker-rs/rs_impl/build.bat
+++ tracker-rs/rs_impl/build.bat
@@ -1,2 +1,6 @@
-cd "%VS120COMNTOOLS%\..\..\VC"
+IF DEFINED %VS120COMNTOOLS%] (
+	cd "%VS120COMNTOOLS%\..\..\VC"
+	) ELSE (
+	cd "%VS140COMNTOOLS%\..\..\VC"
+	)
 vcvarsall x64 && cd %~dp0 && CL /nologo /Ox /DUNICODE /D_UNICODE /MT /I"%RSSDK_DIR%\opensource\include" ftnoir_tracker_rs_impl.cpp udp_sender.cpp "%RSSDK_DIR%\opensource\src\libpxc\libpxc.cpp" /link ADVAPI32.LIB Ws2_32.lib /SUBSYSTEM:CONSOLE /OUT:bin\opentrack-tracker-rs-impl.exe 
