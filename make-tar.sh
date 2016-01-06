1baa26f8c13462ebc424605420d8e60a1977e3a8
Stanislaw Halik <sthalik@misaki.pl>
2016-01-06 20:24:11 +0100

    cmake: don't upload tarball for branch builds

--- make-tar.sh
+++ make-tar.sh
@@ -4,22 +4,28 @@ prefix="$1"
 filename="$2"
 bin="$3"
 
+branch="$(git rev-parse --abbrev-ref HEAD)"
+
 cmake --build "$bin" --target install -- -j4 || exit 1
 
-if  : &&
+if : &&
     cd $(dirname -- "${prefix}") &&
     zip -9r "${filename}" $(basename "${prefix}")
 then
-    case "$USER,$(uname -s)" in
-        # for the script see https://github.com/andreafabrizi/Dropbox-Uploader
-        sthalik,CYGWIN_*)
-            dropbox_uploader.sh -p upload "$filename" /
-            bn="$(basename -- "$filename")"
-            l="$(dropbox_uploader.sh -q share /"$bn")"
-            test -n "$l" && echo -n "$l" | putclip
-            echo $l
-            echo -ne '\a' ;;
-        *) ls -lh -- "${filename}" ;;
+    case "$branch" in
+    unstable)
+        case "$USER,$(uname -s)" in
+            # for the script see https://github.com/andreafabrizi/Dropbox-Uploader
+            sthalik,CYGWIN_*)
+                dropbox_uploader.sh -p upload "$filename" /
+                bn="$(basename -- "$filename")"
+                l="$(dropbox_uploader.sh -q share /"$bn")"
+                test -n "$l" && echo -n "$l" | putclip
+                echo $l
+                echo -ne '\a' ;;
+            *) ls -lh -- "${filename}" ;;
+        esac ;;
+    *) ;;
     esac
 else
     rm -fv -- "${filename}"
