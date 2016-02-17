38306b389951c69d050e3e4b929b442b5bd0ada5
Stanislaw Halik <sthalik@misaki.pl>
2016-02-17 04:40:53 +0100

    tracker/pt: protect get_n_points()
    There's a race here since further accesses to the points array aren't
    protected by a mutex in the extractor class.
    
    There's no race in "get_points()" in the extractor since it's only used
    in same thread where updates take place.

--- tracker-pt/point_extractor.h
+++ tracker-pt/point_extractor.h
@@ -23,6 +23,7 @@ public:
     // WARNING: returned reference is valid as long as object
     const std::vector<cv::Vec2f> &extract_points(cv::Mat &frame);
     const std::vector<cv::Vec2f>& get_points() { QMutexLocker l(&mtx); return points; }
+    int get_n_points() const { QMutexLocker l(&mtx); return points.size(); }
     PointExtractor();
     
     settings_pt s;
