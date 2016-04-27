fb7e86d02e1d9a15dedad048aa477c1985423057
Stanislaw Halik <sthalik@misaki.pl>
2016-04-27 08:00:58 +0200

    tracker/pt: sensible max point size

--- tracker-pt/point_extractor.cpp
+++ tracker-pt/point_extractor.cpp
@@ -118,7 +118,7 @@ const std::vector<cv::Vec2f>& PointExtractor::extract_points(cv::Mat& frame)
         const int sz = hist.cols * hist.rows;
         int val = 0;
         int cnt = 0;
-        constexpr int min_pixels = 250;
+        constexpr int min_pixels = 20;
         const auto pixels_to_include = std::max<int>(0, min_pixels * s.threshold/100.);
         auto ptr = reinterpret_cast<const float*>(hist.ptr(0));
         for (int i = sz-1; i >= 0; i--)
