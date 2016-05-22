2c6baff24800711e61cdbbc43e97bfc270e49419
Stanislaw Halik <sthalik@misaki.pl>
2016-05-22 11:52:12 +0200

    compat/sleep: include headers outside namespace scope

--- opentrack-compat/sleep.hpp
+++ opentrack-compat/sleep.hpp
@@ -1,20 +1,20 @@
 #pragma once
 
+#ifdef _WIN32
+#   include <windows.h>
+#else
+#   include <unistd.h>
+#endif
+
 namespace portable
 {
 #ifdef _WIN32
-#   include <windows.h>
-
-    template<typename = void>
-    void sleep(unsigned milliseconds)
+    inline void sleep(unsigned milliseconds)
     {
         Sleep(milliseconds);
     }
 #else
-    #include <unistd.h>
-
-    template<typename = void>
-    void sleep(unsigned milliseconds)
+    inline void sleep(unsigned milliseconds)
     {
         usleep(milliseconds * 1000U); // takes microseconds
     }
