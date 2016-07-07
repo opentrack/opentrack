f98dcfc2360302753f9a8bf66ee8ccfe4366f0e7
Stanislaw Halik <sthalik@misaki.pl>
2016-07-07 15:44:20 +0200

    tracker/rs: use static linkage
    There's no reason not to link statically the impl library.
    
    As suggested by @ph0b, use subdirs for each arch. This helps with object
    file clobber.

--- tracker-rs/rs_impl/build_wrapped.bat
+++ tracker-rs/rs_impl/build_wrapped.bat
@@ -1,4 +1,6 @@
-@IF DEFINED VS150COMNTOOLS (
+@echo off
+
+IF DEFINED VS150COMNTOOLS (
 	set vs_dir="%VS150COMNTOOLS%\..\..\VC"
     set vs_64=x86_amd64
 	) ELSE (
@@ -6,5 +8,16 @@
     set vs_64=amd64
 	)
 
-cmd /c %~dp0\build_x86.bat
-cmd /c %~dp0\build_amd64.bat
+
+cd /d "%~dp0"
+if %errorlevel% neq 0 exit 1
+
+set rs_objs=ftnoir_tracker_rs_impl.obj libpxc.obj
+
+set rs_subdir=ia32
+set vs_var=x86
+cmd /c "call .\build_common.bat"
+
+set rs_subdir=amd64
+set vs_var=%vs_64%
+cmd /c "call .\build_common.bat"
