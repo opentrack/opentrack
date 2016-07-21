72f7a1b07d393e6cfdbac1ecb168a703919fd03e
Stanislaw Halik <sthalik@misaki.pl>
2016-07-21 14:30:09 +0200

    api/is-window-visible: check for more window positions
    Having the window center invisible making the feed not refresh is
    confusing. Also check for all the corners.

--- opentrack/is-window-visible.hpp
+++ opentrack/is-window-visible.hpp
@@ -4,6 +4,8 @@
 #include <QWidget>
 #include <QRect>
 #include <QDebug>
+#include <QPoint>
+#include <initializer_list>
 
 template<typename=void>
 bool is_window_visible(const QWidget* widget)
@@ -13,11 +15,22 @@ bool is_window_visible(const QWidget* widget)
     if (!window)
         return true;
 
-    const QPoint p = widget->mapToGlobal(widget->pos());
+    const QPoint p = widget->mapToGlobal(QPoint(0, 0));
     const QSize s = widget->size();
-    POINT pt = { p.x() + s.width()/2, p.y() + s.height()/2 };
 
-    return WindowFromPoint(pt) == (HWND) widget->winId();
+    const std::initializer_list<POINT> points =
+    {
+        { p.x(), p.y() },
+        { p.x() + s.width(), p.y() },
+        { p.x() + s.width(), p.y() + s.height() },
+        { p.x(), p.y() + s.height() },
+        { p.x() + s.width()/2, p.y() + s.height()/2 },
+    };
+
+    for (const POINT& pt : points)
+        if (WindowFromPoint(pt) == (HWND) widget->winId())
+            return true;
+    return false;
 }
 
 #else
