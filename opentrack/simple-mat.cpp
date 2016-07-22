256b5d2204bcae3db62c86cdf4774bdaa448c6b8
Stanislaw Halik <sthalik@misaki.pl>
2016-07-22 07:53:33 +0200

    api/simple-mat: don't use a temporary array

--- opentrack/simple-mat.cpp
+++ opentrack/simple-mat.cpp
@@ -43,22 +43,20 @@ rmat euler_to_rmat(const double* input)
     const auto c3 = cos(B);
     const auto s3 = sin(B);
 
-    double foo[] = {
-        // z
-        c1 * c2,
-        c1 * s2 * s3 - c3 * s1,
-        s1 * s3 + c1 * c3 * s2,
-        // y
-        c2 * s1,
-        c1 * c3 + s1 * s2 * s3,
-        c3 * s1 * s2 - c1 * s3,
-        // x
-        -s2,
-        c2 * s3,
-        c2 * c3
-    };
-
-    return dmat<3, 3>(foo);
+    return dmat<3, 3>(
+                // z
+                c1 * c2,
+                c1 * s2 * s3 - c3 * s1,
+                s1 * s3 + c1 * c3 * s2,
+                // y
+                c2 * s1,
+                c1 * c3 + s1 * s2 * s3,
+                c3 * s1 * s2 - c1 * s3,
+                // x
+                -s2,
+                c2 * s3,
+                c2 * c3
+                );
 }
 
 } // end ns euler
