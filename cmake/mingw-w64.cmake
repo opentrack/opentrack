8e3c3358a09c3c6974645fb411e4f316c734e3db
Stanislaw Halik <sthalik@misaki.pl>
2016-07-25 11:04:00 +0200

    cmake/mingw-w64: default lto gzip -9 lto stream files
    Highly loaded drive slows down build otherwise.

--- cmake/mingw-w64.cmake
+++ cmake/mingw-w64.cmake
@@ -39,7 +39,7 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 # oldest CPU supported here is Northwood-based Pentium 4. -sh 20150811
 set(fpu "-ffast-math -mfpmath=both -mstackrealign -ftree-vectorize")
 set(cpu "-O3 -march=pentium4 -mtune=corei7-avx -msse -msse2 -mno-sse3 -mno-avx -frename-registers -fno-PIC")
-set(lto "-flto -fuse-linker-plugin -flto-compression-level=3 -flto-partition=balanced -fno-ipa-pta")
+set(lto "-flto -fuse-linker-plugin -flto-compression-level=9 -flto-partition=balanced -fno-ipa-pta")
 
 set(_CFLAGS " -fvisibility=hidden")
 set(_CXXFLAGS "${_CFLAGS}")
