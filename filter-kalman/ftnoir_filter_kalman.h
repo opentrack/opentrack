e24ba82d73cb565a9bac07592840b16389fbb0e6
DaMichel <mw.pub@welter-4d.de>
2016-07-29 10:38:22 +0200

    kalman filter revised: noise model change, switch to Eigen3, implemented process noise covariance scaling adaptive KF, augmented akf with deadzone filter as second filter stage

--- filter-kalman/ftnoir_filter_kalman.h
+++ filter-kalman/ftnoir_filter_kalman.h
@@ -10,40 +10,115 @@
 
 #include "ui_ftnoir_kalman_filtercontrols.h"
 #include "opentrack/plugin-api.hpp"
-#include <opencv2/core.hpp>
-#include <opencv2/video/tracking.hpp>
-#include <vector>
+
+#include <atomic>
+#include <Eigen/Core>
+#include <Eigen/LU>
+
 #include <QString>
-#include <QElapsedTimer>
 #include <QWidget>
+
 #include "opentrack-compat/options.hpp"
 using namespace options;
 #include "opentrack-compat/timer.hpp"
 
-struct settings : opts {
-    value<int> noise_stddev_slider;
-    // slider for noise_stddev goes 0->(mult_noise_stddev * 100)
-    static constexpr double mult_noise_stddev = .5;
-    settings() : opts("kalman-filter"), noise_stddev_slider(b, "noise-stddev", 40)
+
+static constexpr int NUM_STATE_DOF = 12;
+static constexpr int NUM_MEASUREMENT_DOF = 6;
+using Matrix = Eigen::MatrixXd; // variable size, heap allocated, double valued
+// These vectors are compile time fixed size, stack allocated
+using StateVector = Eigen::Matrix<double, NUM_STATE_DOF, 1>;
+using PoseVector = Eigen::Matrix<double, NUM_MEASUREMENT_DOF, 1>;
+
+struct KalmanFilter
+{
+    Matrix
+        measurement_noise_cov,
+        process_noise_cov,
+        state_cov,
+        state_cov_prior,
+        kalman_gain,
+        transition_matrix,
+        measurement_matrix;
+    StateVector
+        state,
+        state_prior;
+    PoseVector
+        innovation;
+    void init();
+    void time_update();
+    void measurement_update(const PoseVector &measurement);
+};
+
+struct KalmanProcessNoiseScaler
+{
+    Matrix
+        innovation_cov_estimate,
+        base_cov; // baseline (unscaled) process noise covariance matrix
+    void init();
+    void update(KalmanFilter &kf, double dt);
+};
+
+
+struct DeadzoneFilter
+{
+    PoseVector
+        last_output,
+        dz_size;
+    DeadzoneFilter() :
+        last_output(PoseVector::Zero()),
+        dz_size(PoseVector::Zero())
     {}
+    void reset() {
+        last_output = PoseVector::Zero();
+    }
+    PoseVector filter(const PoseVector &input);
+};
+
+
+struct settings : opts {
+    value<int> noise_rot_slider_value;
+    value<int> noise_pos_slider_value;
+
+    static constexpr double adaptivity_window_length = 0.5; // seconds
+    static constexpr double deadzone_scale = 2.;
+    static constexpr double deadzone_exponent = 4.0;
+    // these values worked best for me (MW) when taken with acompanying measured noise stddev of ca 0.1 (rot) and 0.01 (pos). 
+    static constexpr double process_sigma_pos = 0.05;
+    static constexpr double process_simga_rot = 0.5;
+
+    static double map_slider_value(int v)
+    {
+        return std::pow(10., v * 0.04 - 3.);
+    }
+
+    settings() :  
+        opts("kalman-filter"), 
+        noise_rot_slider_value(b, "noise-rotation-slider", 40), 
+        noise_pos_slider_value(b, "noise-position-slider", 40)
+    {}
+
 };
 
 class FTNoIR_Filter : public IFilter
 {
+    PoseVector do_kalman_filter(const PoseVector &input, double dt, bool new_input);
+    void fill_transition_matrix(double dt);
+    void fill_process_noise_cov_matrix(Matrix &target, double dt) const;
 public:
     FTNoIR_Filter();
     void reset();
     void filter(const double *input, double *output);
-    // Set accel_stddev assuming moving 0.0->accel in dt_ is 3 stddevs: (accel*4/dt_^2)/3.
-    static constexpr double dt_ = .4;
-    static constexpr double accel = 60.;
-    static constexpr double accel_stddev = (accel*4/(dt_*dt_))/3.0;
-    cv::KalmanFilter kalman;
-    double last_input[6];
+    PoseVector last_input;
     Timer timer;
     bool first_run;
+    double dt_since_last_input;
     settings s;
-    int prev_slider_pos;
+    KalmanFilter kf;
+    KalmanProcessNoiseScaler kf_adaptive_process_noise_cov;
+    PoseVector minimal_state_var;
+    DeadzoneFilter dz_filter;
+    int prev_slider_pos[2];
 };
 
 class FTNoIR_FilterDll : public Metadata
@@ -57,16 +132,12 @@ class FilterControls: public IFilterDialog
 {
     Q_OBJECT
 public:
-    FilterControls() {
-        ui.setupUi(this);
-        connect(ui.buttonBox, SIGNAL(accepted()), this, SLOT(doOK()));
-        connect(ui.buttonBox, SIGNAL(rejected()), this, SLOT(doCancel()));
-        tie_setting(s.noise_stddev_slider, ui.noise_slider);
-    }
+    FilterControls();
     Ui::KalmanUICFilterControls ui;
     void register_filter(IFilter*) override {}
     void unregister_filter() override {}
     settings s;
+    FTNoIR_Filter *filter;
 public slots:
     void doOK();
     void doCancel();
