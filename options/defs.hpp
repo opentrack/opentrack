190ec92ac66c4b2c44d692be9b861ec98853355a
Stanislaw Halik <sthalik@misaki.pl>
2016-09-16 17:41:53 +0200

    options: add QStringLiteral define

--- options/defs.hpp
+++ options/defs.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <QString>
+
 #define OPENTRACK_CONFIG_FILENAME_KEY "settings-filename"
 #define OPENTRACK_DEFAULT_CONFIG "default.ini"
+#define OPENTRACK_DEFAULT_CONFIG_Q QStringLiteral("default.ini")
 #define OPENTRACK_ORG "opentrack-2.3"
