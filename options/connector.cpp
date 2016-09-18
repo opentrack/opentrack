548c9549cd3e408df63426090088b08d72c2658e
Stanislaw Halik <sthalik@misaki.pl>
2016-09-18 11:59:14 +0200

    add missing copyright terms
    Due to fucking AT&T vs BSD suit copyright defaults to all rights
    reserved. Fuck Berne Convention as well.

--- options/connector.cpp
+++ options/connector.cpp
@@ -1,3 +1,11 @@
+/* Copyright (c) 2016, Stanislaw Halik <sthalik@misaki.pl>
+
+ * Permission to use, copy, modify, and/or distribute this
+ * software for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice and this permission
+ * notice appear in all copies.
+ */
+
 #include "compat/util.hpp"
 #include "connector.hpp"
 #include "value.hpp"
--- options/value.cpp
+++ options/value.cpp
@@ -1,5 +1,12 @@
-#include "value.hpp"
+/* Copyright (c) 2015-2016, Stanislaw Halik <sthalik@misaki.pl>
 
+ * Permission to use, copy, modify, and/or distribute this
+ * software for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice and this permission
+ * notice appear in all copies.
+ */
+
+#include "value.hpp"
 
 namespace options {
 
