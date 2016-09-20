e258767f32bbafef9f862209aa8868342e946975
Stanislaw Halik <sthalik@misaki.pl>
2016-09-20 22:12:07 +0200

    compat/sleep: fix namespace fuckup

--- opentrack-compat/sleep.hpp
+++ opentrack-compat/sleep.hpp
@@ -1,20 +1,21 @@
 #pragma once
 
-namespace portable
-{
 #ifdef _WIN32
 #   include <windows.h>
-
+namespace portable
+{
     inline void sleep(unsigned milliseconds)
     {
         Sleep(milliseconds);
     }
+}
 #else
-    #include <unistd.h>
+#   include <unistd.h>
+namespace portable {
 
     inline void sleep(unsigned milliseconds)
     {
         usleep(milliseconds * 1000U); // takes microseconds
     }
-#endif
 }
+#endif
