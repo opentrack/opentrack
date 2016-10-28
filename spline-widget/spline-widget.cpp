opentrack/opentrack

commit a3b890b9b4d8bc14c662cd0b3a3362e784b35593

spline-widget: use round, not trunc when converting pixel to val

spline-widget/spline-widget.cpp
@@ -543,14 +543,16 @@ QPointF spline_widget::pixel_coord_to_point(const QPoint& point)
 
     if (snap_x > 0)
     {
+        x += snap_x / 2.;
         x -= std::fmod(x, snap_x);
-        const volatile int x_ = int(x * c);
+        const volatile int x_ = iround(x * c);
         x = x_ / double(c);
     }
     if (snap_y > 0)
     {
+        y += snap_y / 2.;
         y -= std::fmod(y, snap_y);
-        const volatile int y_ = int(y * c);
+        const volatile int y_ = iround(y * c);
         y = y_ / double(c);
     }
 

commit bdffe51bade6b35e46bb5f78cb3be77c8310e4c7

spline-widget: fix rounding bug introduced in a3b890b9b4
Since we're already adding snap_x/2, truncation is the correct
choice here.

spline-widget/spline-widget.cpp
@@ -545,14 +545,14 @@ QPointF spline_widget::pixel_coord_to_point(const QPoint& point)
     {
         x += snap_x / 2.;
         x -= std::fmod(x, snap_x);
-        const volatile int x_ = iround(x * c);
+        const volatile int x_ = int(x * c);
         x = x_ / double(c);
     }
     if (snap_y > 0)
     {
         y += snap_y / 2.;
         y -= std::fmod(y, snap_y);
-        const volatile int y_ = iround(y * c);
+        const volatile int y_ = int(y * c);
         y = y_ / double(c);
     }
 