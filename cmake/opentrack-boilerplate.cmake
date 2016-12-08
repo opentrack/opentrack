945ef5e00d2ab901de815fd495fb9054acf13ced
Stanislaw Halik <sthalik@misaki.pl>
2016-12-08 06:34:55 +0100

    cmake: fix typo
    Wow, it's been there for a while

--- cmake/opentrack-boilerplate.cmake
+++ cmake/opentrack-boilerplate.cmake
@@ -199,7 +199,7 @@ function(opentrack_boilerplate n)
 
     if(NOT arg_STATIC)
         string(REGEX REPLACE "^opentrack-" "" n_ "${n}")
-        string(REGEX REPLACE "^(tracker|filter-proto)-" "" n_ "${n_}")
+        string(REGEX REPLACE "^(tracker|filter|proto)-" "" n_ "${n_}")
         string(REPLACE "-" "_" n_ ${n_})
         target_compile_definitions(${n} PRIVATE "BUILD_${n_}")
 
