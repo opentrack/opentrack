d2264ce6cd1af385520bb1c3dbbd642359994694
Stanislaw Halik <sthalik@misaki.pl>
2017-02-23 06:52:20 +0100

    gui/process-detector: fix user interface layout issues
    It can still break on some exotic styles like OSX.

--- gui/process-detector-fancy-table.cpp
+++ gui/process-detector-fancy-table.cpp
@@ -6,8 +6,9 @@ void FancyTable::resizeEvent(QResizeEvent* e)
 {
     QTableView::resizeEvent(e);
     int w = width();
+    setContentsMargins(QMargins(0, 0, 0, 0));
+    w -= 64;
     setColumnWidth(2, 32);
-    w -= 32;
     setColumnWidth(0, w / 2);
     setColumnWidth(1, w / 2);
     horizontalHeader()->setSectionResizeMode(QHeaderView::Fixed);
