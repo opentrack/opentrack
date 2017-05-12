f7ab2dc5a54c573cd7408a68cb1e93174827719e
Stanislaw Halik <sthalik@misaki.pl>
2017-05-12 15:44:05 +0200

    options: split up value.hpp header
    Also combine the traits classes and make them more useful.

---
+++ options/base-value.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "bundle.hpp"
+#include "slider.hpp"
+#include "connector.hpp"
+
+#include "export.hpp"
+
+#include <QObject>
+#include <QString>
+#include <QList>
+#include <QPointF>
+#include <QVariant>
+
+#include <typeindex>
+
+#define OPENTRACK_DEFINE_SLOT(t) void setValue(t datum) { store(datum); }
+#define OPENTRACK_DEFINE_SIGNAL(t) void valueChanged(t) const
+
+namespace options {
+
+class OTR_OPTIONS_EXPORT base_value : public QObject
+{
+    Q_OBJECT
+    friend class detail::connector;
+
+    using comparator = bool(*)(const QVariant& val1, const QVariant& val2);
+public:
+    QString name() const { return self_name; }
+    base_value(bundle b, const QString& name, comparator cmp, std::type_index type_idx);
+    ~base_value() override;
+signals:
+    OPENTRACK_DEFINE_SIGNAL(double);
+    OPENTRACK_DEFINE_SIGNAL(float);
+    OPENTRACK_DEFINE_SIGNAL(int);
+    OPENTRACK_DEFINE_SIGNAL(bool);
+    OPENTRACK_DEFINE_SIGNAL(const QString&);
+    OPENTRACK_DEFINE_SIGNAL(const slider_value&);
+    OPENTRACK_DEFINE_SIGNAL(const QPointF&);
+    OPENTRACK_DEFINE_SIGNAL(const QVariant&);
+
+    OPENTRACK_DEFINE_SIGNAL(const QList<double>&);
+    OPENTRACK_DEFINE_SIGNAL(const QList<float>&);
+    OPENTRACK_DEFINE_SIGNAL(const QList<int>&);
+    OPENTRACK_DEFINE_SIGNAL(const QList<bool>&);
+    OPENTRACK_DEFINE_SIGNAL(const QList<QString>&);
+    OPENTRACK_DEFINE_SIGNAL(const QList<slider_value>&);
+    OPENTRACK_DEFINE_SIGNAL(const QList<QPointF>&);
+protected:
+    bundle b;
+    QString self_name;
+    comparator cmp;
+    std::type_index type_index;
+
+    void store(const QVariant& datum);
+
+    template<typename t>
+    void store(const t& datum)
+    {
+        b->store_kv(self_name, QVariant::fromValue(datum));
+    }
+
+public slots:
+    OPENTRACK_DEFINE_SLOT(double)
+    OPENTRACK_DEFINE_SLOT(int)
+    OPENTRACK_DEFINE_SLOT(bool)
+    OPENTRACK_DEFINE_SLOT(const QString&)
+    OPENTRACK_DEFINE_SLOT(const slider_value&)
+    OPENTRACK_DEFINE_SLOT(const QPointF&)
+    OPENTRACK_DEFINE_SLOT(const QVariant&)
+
+    OPENTRACK_DEFINE_SLOT(const QList<double>&)
+    OPENTRACK_DEFINE_SLOT(const QList<float>&)
+    OPENTRACK_DEFINE_SLOT(const QList<int>&)
+    OPENTRACK_DEFINE_SLOT(const QList<bool>&)
+    OPENTRACK_DEFINE_SLOT(const QList<QString>&)
+    OPENTRACK_DEFINE_SLOT(const QList<slider_value>&)
+    OPENTRACK_DEFINE_SLOT(const QList<QPointF>&)
+
+    virtual void reload() = 0;
+    virtual void bundle_value_changed() const = 0;
+    virtual void set_to_default() = 0;
+};
+
+} //ns options
---
+++ options/value-traits.hpp
@@ -0,0 +1,50 @@
+#include "export.hpp"
+
+#include "compat/functional.hpp"
+#include "slider.hpp"
+
+#include <QString>
+
+#include <type_traits>
+
+namespace options {
+namespace detail {
+
+template<typename t, typename u = t, typename Enable = void>
+struct default_value_traits
+{
+    using element_type = remove_qualifiers<t>;
+    using value_type = u;
+
+    static inline value_type from_value(const value_type& val, const value_type&) { return val; }
+    static inline value_type from_storage(const element_type& x) { return static_cast<value_type>(x); }
+    static inline element_type to_storage(const value_type& val) { return static_cast<element_type>(val); }
+};
+
+template<typename t, typename u = t, typename Enable = void>
+struct value_traits : default_value_traits<t, u, Enable>
+{
+};
+
+template<>
+struct value_traits<slider_value> : default_value_traits<slider_value>
+{
+    static inline slider_value from_value(const slider_value& val, const slider_value& def)
+    {
+        return slider_value(val.cur(), def.min(), def.max());
+    }
+};
+
+// Qt uses int a lot in slots so use it for all enums
+template<typename t>
+struct value_traits<t, t, std::enable_if_t<std::is_enum<t>::value>> : public default_value_traits<int, t>
+{
+};
+
+template<>
+struct value_traits<float> : public default_value_traits<float, double, void>
+{
+};
+
+} // ns detail
+} // ns options
