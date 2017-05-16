opentrack/opentrack

commit 632cd5bf7778cb9062633f8d27ecd6aadcaa5d28

filter/accela: punt if running ahead of the input value

filter-accela/ftnoir_filter_accela.cpp
@@ -32,6 +32,7 @@ static inline constexpr T signum(T x)
 }
 
 template<int N = 3, typename F>
+OTR_NEVER_INLINE
 static void do_deltas(const double* deltas, double* output, double alpha, double& smoothed, F&& fun)
 {
     double norm[N];
@@ -152,6 +153,12 @@ void accela::filter(const double* input, double *output)
     {
         output[k] *= dt;
         output[k] += last_output[k];
+
+        if (signum(last_output[k] - output[k]) < 0)
+            output[k] = std::fmax(input[k], output[k]);
+        else
+            output[k] = std::fmin(input[k], output[k]);
+
         last_output[k] = output[k];
     }
 }

commit 2b755b20caeae9f3b2ce4471e872204335e9db70

filter/accela: fix clamp regression
Clean up after 632cd5bf7778cb9062633f8d27ecd6aadcaa5d28.

The condition was inverting, causing great pain and woe.

Issue: #620.

filter-accela/ftnoir_filter_accela.cpp
@@ -154,7 +154,7 @@ void accela::filter(const double* input, double *output)
         output[k] *= dt;
         output[k] += last_output[k];
 
-        if (signum(last_output[k] - output[k]) < 0)
+        if (signum(last_output[k] - output[k]) >= 0)
             output[k] = std::fmax(input[k], output[k]);
         else
             output[k] = std::fmin(input[k], output[k]);