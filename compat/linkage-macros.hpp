29857b87efdec4ed6a4adb8162d2428460eec53a
Stanislaw Halik <sthalik@misaki.pl>
2017-06-07 03:55:46 +0200

    cmake: cleanup
    We now inherit parent process' console. No need for
    the cmake cache option.

--- compat/linkage-macros.hpp
+++ compat/linkage-macros.hpp
@@ -7,9 +7,3 @@
 #   define OTR_GENERIC_EXPORT __attribute__ ((visibility ("default")))
 #   define OTR_GENERIC_IMPORT
 #endif
-
-#if defined _MSC_VER
-#   define OTR_GENERIC_TEMPLATE
-#else
-#   define OTR_GENERIC_TEMPLATE __attribute__ ((visibility ("default")))
-#endif
