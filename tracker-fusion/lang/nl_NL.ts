a6d54f7ed35d972793e819eae23eadc17e4bca50
Stanislaw Halik <sthalik@misaki.pl>
2017-06-19 16:11:15 +0200

    tracker/fusion: add untranslated stubs

---
+++ tracker-fusion/lang/nl_NL.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>fusion_dialog</name>
+    <message>
+        <location filename="../fusion.cpp" line="+185"/>
+        <source>Fusion tracker only works when distinct trackers are selected for rotation and position.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fusion_metadata</name>
+    <message>
+        <location filename="../fusion.h" line="+55"/>
+        <source>Fusion</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fusion_tracker</name>
+    <message>
+        <location filename="../fusion.cpp" line="-134"/>
+        <source>Fusion tracker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <source>Select different trackers for rotation and position.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+55"/>
+        <source>Select rotation and position trackers.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fusion_ui</name>
+    <message>
+        <location filename="../fusion.ui" line="+23"/>
+        <source>Fusion</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+28"/>
+        <source>Set distinct trackers for rotation and position input.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Configure the trackers on the main window. It&apos;s required that they&apos;re both distinct, and both are set to something.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+34"/>
+        <source>Rotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+35"/>
+        <source>Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
