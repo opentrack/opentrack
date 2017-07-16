1ac33e5d5aea97448c71ea58b12492ae219e88f1
Stanislaw Halik <sthalik@misaki.pl>
2017-07-16 15:41:15 +0200

    tracker/s2bot: add clean i18n files
    Related-to: #650

---
+++ tracker-s2bot/lang/nl_NL.ts
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>UI_s2bot_dialog</name>
+    <message>
+        <location filename="../s2bot-controls.ui" line="+17"/>
+        <source>Tracker settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+31"/>
+        <source>Update frequency (Hz)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>Axis order</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>output yaw</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <location line="+24"/>
+        <location line="+31"/>
+        <source>input yaw</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-50"/>
+        <location line="+24"/>
+        <location line="+31"/>
+        <source>input pitch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-50"/>
+        <location line="+24"/>
+        <location line="+31"/>
+        <source>input roll</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-50"/>
+        <location line="+24"/>
+        <location line="+31"/>
+        <source>input bearing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-23"/>
+        <source>output pitch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+31"/>
+        <source>output roll</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Add to axis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>yaw</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <location line="+43"/>
+        <location line="+29"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-67"/>
+        <location line="+43"/>
+        <location line="+29"/>
+        <source>+90</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-67"/>
+        <location line="+43"/>
+        <location line="+29"/>
+        <source>-90</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-67"/>
+        <location line="+43"/>
+        <location line="+29"/>
+        <source>+180</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-67"/>
+        <location line="+43"/>
+        <location line="+29"/>
+        <source>-180</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-64"/>
+        <source>pitch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>roll</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>meta_s2bot</name>
+    <message>
+        <location filename="../ftnoir_tracker_s2bot.h" line="+69"/>
+        <source>S2Bot receiver</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
