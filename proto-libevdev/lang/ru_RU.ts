abe860c0265a8a98e7c3d2fa05f769cf779b52bf
Stanislaw Halik <sthalik@misaki.pl>
2017-07-29 06:12:36 +0200

    proto/{libevdev,wine}: add stub i18n
    Was breaking make(1).
    
    Issue: #661

---
+++ proto-libevdev/lang/ru_RU.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>UICLibevdevControls</name>
+    <message>
+        <location filename="../ftnoir_libevdev_controls.ui" line="+17"/>
+        <source>VJoy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Make sure rw for /dev/input/uinput!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+32"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+25"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ proto-wine/lang/nl_NL.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>UICFTControls</name>
+    <message>
+        <location filename="../ftnoir_winecontrols.ui" line="+17"/>
+        <source>FreeTrack settings FaceTrackNoIR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+48"/>
+        <source>There are no settings necessary for the Wine protocol.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+47"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+25"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
