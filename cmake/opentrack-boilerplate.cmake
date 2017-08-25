945273ac61a699f2c05343f32259c514d359fb65
Stanislaw Halik <sthalik@misaki.pl>
2017-08-25 11:49:10 +0200

    cmake: fix typo

--- cmake/opentrack-boilerplate.cmake
+++ cmake/opentrack-boilerplate.cmake
@@ -118,7 +118,7 @@ function(otr_i18n_for_target_directory n)
     foreach(i ${opentrack_all-translations})
         set(t "${CMAKE_CURRENT_SOURCE_DIR}/lang/${i}.ts")
         set_property(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}" PROPERTY CLEAN_NO_CUSTOM 1)
-        if(NOT opentrack_disable-i17n-update)
+        if(NOT opentrack_disable-i18n-update)
             add_custom_command(OUTPUT "${t}"
                 COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_CURRENT_SOURCE_DIR}/lang"
                 COMMAND "${lupdate-binary}" -silent -recursive -no-obsolete -locations relative . -ts "${t}"
