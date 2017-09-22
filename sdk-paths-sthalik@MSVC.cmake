35b1259513e5870ba06e3c3626a703973f8d8fb3
Stanislaw Halik <sthalik@misaki.pl>
2017-09-22 14:12:32 +0200

    cmake: fix my config

--- sdk-paths-sthalik@MSVC.cmake
+++ sdk-paths-sthalik@MSVC.cmake
@@ -1,4 +1,4 @@
-set(Qt5_DIR "D:/dev/qt-5.8.0-msvc2015/lib/cmake/Qt5")
+set(Qt5_DIR "D:/dev/qt-5.6.2/lib/cmake/Qt5")
 set(__depdir "d:/dev/opentrack-depends/")
 set(OpenCV_DIR "${__depdir}/opencv/build-msvc15")
 
@@ -16,7 +16,7 @@ set(SDK_RIFT_080 "${__depdir}/LibOVR-080/build-msvc15" CACHE PATH "")
 set(SDK_RIFT_140 "${__depdir}/LibOVR-140/build-msvc15/install" CACHE PATH "")
 
 set(SDK_VALVE_STEAMVR "${__depdir}/steamvr" CACHE PATH "")
-set(SDK_TOBII_EYEX "${__depdir}/Tobii EyeX" CACHE PATH "")
+set(SDK_TOBII_EYEX "${__depdir}/Tobii-EyeX" CACHE PATH "")
 set(SDK_VJOYSTICK "${__depdir}/vjoystick" CACHE PATH "")
 
 set(SDK_REALSENSE "D:/RSSDK-R2" CACHE PATH "")
