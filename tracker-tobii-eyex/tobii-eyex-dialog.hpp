2396ac190a62c30cbc687bb7c19119e1a8fd431e
Stanislaw Halik <sthalik@misaki.pl>
2017-12-02 08:19:25 +0100

    tracker/tobii: TODO

--- tracker-tobii-eyex/tobii-eyex-dialog.hpp
+++ tracker-tobii-eyex/tobii-eyex-dialog.hpp
@@ -6,15 +6,16 @@
 #include "ui_tobii-eyex-dialog.h"
 #include <QObject>
 
-struct tobii_eyex_dialog final : public ITrackerDialog
+class tobii_eyex_dialog final : public ITrackerDialog
 {
-    tobii_eyex_dialog();
-    void register_tracker(ITracker*) override {}
-    void unregister_tracker() override {}
-private:
+    Q_OBJECT
     Ui::tobii_eyex_dialog_widgets ui;
     settings s;
 private slots:
     void do_ok();
     void do_cancel();
+public:
+    tobii_eyex_dialog();
+    void register_tracker(ITracker*) override {}
+    void unregister_tracker() override {}
 };
--- tracker-tobii-eyex/tobii-settings.hpp
+++ tracker-tobii-eyex/tobii-settings.hpp
@@ -23,14 +23,14 @@ struct settings final : public opts
 {
     value<tobii_mode> mode { b, "mode", tobii_snap };
 
-    value<slider_value> snap_speed { b, "snap-speed", slider_value(.1, .05, 1) },
-                        snap_inv_dz { b, "snap-screen-edge-length", slider_value(.35, .1, .5) };
-    value<slider_value> acc_speed { b, "acc-speed", slider_value(.1, .05, 1) },
-                        acc_dz_len { b, "acc-screen-edge-length", slider_value(.1, .1, 1) };
-    value<max_yaw> snap_yaw { b, "snap-max-yaw", y20 },
-                   acc_yaw { b, "acc-max-yaw", y20 };
-    value<max_pitch> snap_pitch { b, "snap-max-pitch", p15 },
-                     acc_pitch { b, "acc-max-pitch", p15 };
+    value<slider_value> snap_speed {b, "snap-speed", slider_value(.1, .05, 1)},
+                        snap_inv_dz {b, "snap-screen-edge-length", slider_value(.35, .1, .5)};
+    value<slider_value> acc_speed {b, "acc-speed", slider_value(.1, .05, 1)},
+                        acc_dz_len {b, "acc-screen-edge-length", slider_value(.1, .1, 1)};
+    value<max_yaw> snap_yaw {b, "snap-max-yaw", y20},
+                   acc_yaw {b, "acc-max-yaw", y20};
+    value<max_pitch> snap_pitch {b, "snap-max-pitch", p15},
+                     acc_pitch {b, "acc-max-pitch", p15};
 
     settings() : opts("tobii-eyex") {}
 };
