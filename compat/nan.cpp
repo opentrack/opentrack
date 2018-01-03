3a987e9ae9aa72f361bb7a549102cb66c5abd268
Stanislaw Halik <sthalik@misaki.pl>
2018-01-03 14:31:41 +0100

    compat/nan: remove pointless extern "C"

--- compat/nan.cpp
+++ compat/nan.cpp
@@ -4,7 +4,7 @@
 #   include <cmath>
 #   define my_isnan std::isnan
 #   define my_isinf std::isinf
-extern "C" OTR_COMPAT_EXPORT __declspec(noinline) bool nanp(double x)
+OTR_COMPAT_EXPORT __declspec(noinline) bool nanp(double x)
 #elif defined __MINGW32__
 
 int __cdecl my_isnan(double)__asm__("__isnan");
@@ -18,17 +18,17 @@ int __cdecl my_fpclassify(double)__asm__("___fpclassify");
 
 #define my_isinf(x) (my_fpclassify(x) == FP_INFINITE)
 
-extern "C" OTR_COMPAT_EXPORT bool __attribute__ ((noinline)) nanp(double x)
+OTR_COMPAT_EXPORT bool __attribute__ ((noinline)) nanp(double x)
 #elif defined __APPLE__
 #   include <math.h>
 #   define my_isnan(x) isnan(x)
 #   define my_isinf(x) isinf(x)
-extern "C" OTR_COMPAT_EXPORT bool __attribute__ ((noinline)) nanp(double x)
+OTR_COMPAT_EXPORT bool __attribute__ ((noinline)) nanp(double x)
 #else
 int my_isnan(double)__asm__("isnan");
 int my_isinf(double)__asm__("isinf");
 
-extern "C" OTR_COMPAT_EXPORT bool __attribute__ ((noinline)) nanp(double x)
+OTR_COMPAT_EXPORT bool __attribute__ ((noinline)) nanp(double x)
 #endif
 {
     return my_isnan(x) || my_isinf(x);
--- compat/nan.hpp
+++ compat/nan.hpp
@@ -3,9 +3,9 @@
 #include "export.hpp"
 
 #if defined(__GNUC__)
-extern "C" OTR_COMPAT_EXPORT bool __attribute__ ((noinline)) nanp(double value);
+OTR_COMPAT_EXPORT bool __attribute__ ((noinline)) nanp(double value);
 #elif defined(_WIN32)
-extern "C" OTR_COMPAT_EXPORT __declspec(noinline) bool nanp(double value);
+OTR_COMPAT_EXPORT __declspec(noinline) bool nanp(double value);
 #else
-extern "C" OTR_COMPAT_EXPORT bool nanp(double value);
+OTR_COMPAT_EXPORT bool nanp(double value);
 #endif
