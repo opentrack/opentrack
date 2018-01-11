6b817869744d319dd8d1aef921c1d90aa42e76a6
Stanislaw Halik <sthalik@misaki.pl>
2018-01-11 14:39:55 +0100

    tracker/pt: modernize member initialization

--- tracker-pt/ftnoir_tracker_pt_settings.h
+++ tracker-pt/ftnoir_tracker_pt_settings.h
@@ -23,28 +23,42 @@ enum pt_color_type
 
 struct settings_pt : opts
 {
-    value<QString> camera_name;
-    value<int> cam_res_x,
-               cam_res_y,
-               cam_fps;
-    value<double> min_point_size, max_point_size;
+    settings_pt() : opts("tracker-pt") {}
 
-    value<int> m01_x, m01_y, m01_z;
-    value<int> m02_x, m02_y, m02_z;
+    value<QString> camera_name { b, "camera-name", "" };
+    value<int> cam_res_x { b, "camera-res-width", 640 },
+               cam_res_y { b, "camera-res-height", 480 },
+               cam_fps { b, "camera-fps", 30 };
+    value<double> min_point_size { b, "min-point-size", 2.5 },
+                  max_point_size { b, "max-point-size", 50 };
 
-    value<int> t_MH_x, t_MH_y, t_MH_z;
+    value<int> m01_x { b, "m_01-x", 0 }, m01_y { b, "m_01-y", 0 }, m01_z { b, "m_01-z", 0 };
+    value<int> m02_x { b, "m_02-x", 0 }, m02_y { b, "m_02-y", 0 }, m02_z { b, "m_02-z", 0 };
 
-    value<int> clip_ty, clip_tz, clip_by, clip_bz;
-    value<int> active_model_panel, cap_x, cap_y, cap_z;
+    value<int> t_MH_x { b, "model-centroid-x", 0 },
+               t_MH_y { b, "model-centroid-y", 0 },
+               t_MH_z { b, "model-centroid-z", 0 };
 
-    value<int> fov;
+    value<int> clip_ty { b, "clip-ty", 40 },
+               clip_tz { b, "clip-tz", 30 },
+               clip_by { b, "clip-by", 70 },
+               clip_bz { b, "clip-bz", 80 };
 
-    value<bool> dynamic_pose;
-    value<int> init_phase_timeout;
-    value<bool> auto_threshold;
-    value<pt_color_type> blob_color;
+    value<int> active_model_panel { b, "active-model-panel", 0 },
+               cap_x { b, "cap-x", 40 },
+               cap_y { b, "cap-y", 60 },
+               cap_z { b, "cap-z", 100 };
 
-    value<slider_value> threshold_slider;
+    value<int> fov { b, "camera-fov", 56 };
+
+    value<bool> dynamic_pose { b, "dynamic-pose-resolution", true };
+    value<int> init_phase_timeout { b, "init-phase-timeout", 250 };
+    value<bool> auto_threshold { b, "automatic-threshold", true };
+    value<pt_color_type> blob_color { b, "blob-color", pt_color_natural };
+
+    value<slider_value> threshold_slider { b, "threshold-slider", slider_value(128, 0, 255) };
+
+#if 0
 
 #define XSTR(x) #x
 
@@ -67,37 +81,5 @@ struct settings_pt : opts
 #undef B
 #undef XSTR
 
-    settings_pt() :
-        opts("tracker-pt"),
-        camera_name(b, "camera-name", ""),
-        cam_res_x(b, "camera-res-width", 640),
-        cam_res_y(b, "camera-res-height", 480),
-        cam_fps(b, "camera-fps", 30),
-        min_point_size(b, "min-point-size", 1),
-        max_point_size(b, "max-point-size", 50),
-        m01_x(b, "m_01-x", 0),
-        m01_y(b, "m_01-y", 0),
-        m01_z(b, "m_01-z", 0),
-        m02_x(b, "m_02-x", 0),
-        m02_y(b, "m_02-y", 0),
-        m02_z(b, "m_02-z", 0),
-        t_MH_x(b, "model-centroid-x", 0),
-        t_MH_y(b, "model-centroid-y", 0),
-        t_MH_z(b, "model-centroid-z", 0),
-        clip_ty(b, "clip-ty", 40),
-        clip_tz(b, "clip-tz", 30),
-        clip_by(b, "clip-by", 70),
-        clip_bz(b, "clip-bz", 80),
-        active_model_panel(b, "active-model-panel", 0),
-        cap_x(b, "cap-x", 40),
-        cap_y(b, "cap-y", 60),
-        cap_z(b, "cap-z", 100),
-        fov(b, "camera-fov", 56),
-        dynamic_pose(b, "dynamic-pose-resolution", true),
-        init_phase_timeout(b, "init-phase-timeout", 250),
-        auto_threshold(b, "automatic-threshold", true),
-        blob_color(b, "blob-color", pt_color_natural),
-        threshold_slider(b, "threshold-slider", slider_value(128, 0, 255))
-    {
-    }
+#endif
 };
