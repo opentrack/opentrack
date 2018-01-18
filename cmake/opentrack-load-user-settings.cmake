e9c4feaec08e3992e8631006d41454d02ac0e12b
Stanislaw Halik <sthalik@misaki.pl>
2018-01-18 19:31:32 +0100

    cmake: make per-user defaults more useful

--- cmake/opentrack-load-user-settings.cmake
+++ cmake/opentrack-load-user-settings.cmake
@@ -8,12 +8,17 @@ if(".${__sdk_username}" STREQUAL ".")
     set(__sdk_username "(I-have-no-name)")
 endif()
 
+if(WIN32)
+    set(__sdk_os "windows")
+else()
+    set(__sdk_os "$ENV{OSTYPE}")
+endif()
+
 include(CMakeDetermineCCompiler)
 include(CMakeDetermineCXXCompiler)
 
-set(__sdk_paths_filename "${CMAKE_SOURCE_DIR}/sdk-paths-${__sdk_username}@${CMAKE_CXX_COMPILER_ID}.cmake")
+set(__sdk_paths_filename "${CMAKE_SOURCE_DIR}/sdk-paths-${__sdk_username}@${CMAKE_CXX_COMPILER_ID}-${__sdk_os}.cmake")
 
 if(EXISTS "${__sdk_paths_filename}")
     include("${__sdk_paths_filename}")
 endif()
-
