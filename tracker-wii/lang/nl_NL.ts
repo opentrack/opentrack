6c2f3452553d68c46c9e38e71b7cc3208acc1c53
Wei Shuai <cpuwolf@gmail.com>
2018-02-09 12:23:02 +0800

    tracker/wii: fix lang pack on NL and RU

--- tracker-wii/lang/nl_NL.ts
+++ tracker-wii/lang/nl_NL.ts
@@ -1,4 +1,293 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="nl_NL">
+<context>
+    <name>PT_metadata</name>
+    <message>
+        <location filename="../ftnoir_tracker_pt.h" line="+95"/>
+        <source>PointTracker 1.1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UICPTClientControls</name>
+    <message>
+        <location filename="../FTNoIR_PT_Controls.ui" line="+23"/>
+        <source>PointTracker Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+38"/>
+        <source>Camera</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Camera settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>°</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <source>Diagonal field of view</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>FPS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Desired capture height</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <location line="+55"/>
+        <location line="+175"/>
+        <location line="+16"/>
+        <source> px</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-233"/>
+        <source>Dynamic pose timeout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Desired capture framerate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source> Hz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <source>Desired capture width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <source>Height</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source> ms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Dynamic pose resolution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Camera settings (when available)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Point extraction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Max size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Threshold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Min size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Intensity threshold for point extraction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+25"/>
+        <source>Automatic threshold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Enable, slider sets point size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Maximum point diameter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Minimum point diameter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>Model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+28"/>
+        <source>Clip</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <location line="+154"/>
+        <location line="+129"/>
+        <source>Model Dimensions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-271"/>
+        <location line="+19"/>
+        <location line="+48"/>
+        <location line="+19"/>
+        <location line="+97"/>
+        <location line="+35"/>
+        <location line="+32"/>
+        <location line="+40"/>
+        <location line="+26"/>
+        <location line="+13"/>
+        <location line="+13"/>
+        <location line="+13"/>
+        <location line="+26"/>
+        <location line="+132"/>
+        <location line="+26"/>
+        <location line="+26"/>
+        <source> mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-511"/>
+        <location line="+116"/>
+        <source>Side</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-65"/>
+        <location line="+132"/>
+        <source>Front</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-107"/>
+        <source>Cap</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+135"/>
+        <source>Custom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <location line="+169"/>
+        <location line="+106"/>
+        <source>z:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-249"/>
+        <location line="+104"/>
+        <location line="+93"/>
+        <source>x:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-132"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Location of the two remaining model points&lt;br/&gt;with respect to the reference point in default pose&lt;/p&gt;&lt;p&gt;Use any units you want, not necessarily centimeters.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <location line="+65"/>
+        <location line="+67"/>
+        <source>y:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-106"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:16pt;&quot;&gt;P&lt;/span&gt;&lt;span style=&quot; font-size:16pt; vertical-align:sub;&quot;&gt;3&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:16pt;&quot;&gt;P&lt;/span&gt;&lt;span style=&quot; font-size:16pt; vertical-align:sub;&quot;&gt;2&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+46"/>
+        <source>Model position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+105"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/opentrack/opentrack/wiki/model-calibration-for-PT-and-Aruco-trackers&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Instructions on the opentrack wiki&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Start calibration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+17"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;FTNoIR PointTracker Plugin&lt;br/&gt;Version 1.1&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;by Patrick Ruoff&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;http://ftnoirpt.sourceforge.net/&quot;&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline; color:#0000ff;&quot;&gt;Manual (external)&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+36"/>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Extracted Points:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Camera Info:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
--- tracker-wii/lang/ru_RU.ts
+++ tracker-wii/lang/ru_RU.ts
@@ -1,4 +1,347 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
+<context>
+    <name>PT_metadata</name>
+    <message>
+        <location filename="../ftnoir_tracker_pt.h" line="+96"/>
+        <source>PointTracker 1.1</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>TrackerDialog_PT</name>
+    <message>
+        <location filename="../ftnoir_tracker_pt_dialog.cpp" line="+130"/>
+        <source>%1 samples. Over %2, good!</source>
+        <translation>Получено %1 образца(-ов). Больше %2, отлично!!</translation>
+    </message>
+    <message>
+        <location line="-8"/>
+        <source>%1 yaw samples. Yaw more to %2 samples for stable calibration.</source>
+        <translation>По оси YAW выполнено: %1 замер(а/ов). Для стабильного результата необходимо не меньше %2</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>%1 pitch samples. Pitch more to %2 samples for stable calibration.</source>
+        <translation>По оси Pitch выполнено: %1 замер(а/ов). Для стабильного результата необходимо не меньше %2</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Stop calibration</source>
+        <translation>Остановить калибровку</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Start calibration</source>
+        <translation>Начать калибровку</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>%1x%2 @ %3 FPS</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>%1 OK!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>%1 BAD!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Tracker offline</source>
+        <translation>Отслеживание отключено</translation>
+    </message>
+</context>
+<context>
+    <name>UICPTClientControls</name>
+    <message>
+        <location filename="../FTNoIR_PT_Controls.ui" line="+23"/>
+        <source>PointTracker Settings</source>
+        <translation>Настройки PointTracker</translation>
+    </message>
+    <message>
+        <location line="+38"/>
+        <source>Camera</source>
+        <translation>Камера</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Camera settings</source>
+        <translation>Настройка камеры</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>°</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <source>Diagonal field of view</source>
+        <translation>Угол обзора камеры</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Width</source>
+        <translation>Ширина</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>FPS</source>
+        <translation>FPS (Кадров в секунду)</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Desired capture height</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <location line="+55"/>
+        <location line="+175"/>
+        <location line="+16"/>
+        <source> px</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="-233"/>
+        <source>Dynamic pose timeout</source>
+        <translation>Динамическая поза (время ожидания)</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Desired capture framerate</source>
+        <translation>Желаемая частота кадров</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source> Hz</source>
+        <translation> Гц</translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <source>Desired capture width</source>
+        <translation>Желаемая ширина захвата</translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <source>Height</source>
+        <translation>Высота</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source> ms</source>
+        <translation> мс</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Dynamic pose (for caps only, never clips)</source>
+        <translation>Динамическая поза (Только для модели &quot;Кепка&quot;)</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Device</source>
+        <translation>Устройство</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Camera settings (when available)</source>
+        <translation>Параметры камеры (если доступно)</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Point extraction</source>
+        <translation>Извлечение точек</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Max size</source>
+        <translation>Макс.размер</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Threshold</source>
+        <translation>Порог</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Min size</source>
+        <translation>Мин.размер</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Intensity threshold for point extraction</source>
+        <translation>Порог интенсивности для извлечения точки</translation>
+    </message>
+    <message>
+        <location line="+25"/>
+        <source>Automatic threshold</source>
+        <translation>Автоматич. порог</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Enable, slider sets point size</source>
+        <translation>Полузнок устанавливает размер точек</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Maximum point diameter</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Minimum point diameter</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>Model</source>
+        <translation>Модель</translation>
+    </message>
+    <message>
+        <location line="+28"/>
+        <source>Clip</source>
+        <translation>Клипса</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <location line="+154"/>
+        <location line="+129"/>
+        <source>Model Dimensions</source>
+        <translation>Размеры модели</translation>
+    </message>
+    <message>
+        <location line="-271"/>
+        <location line="+19"/>
+        <location line="+48"/>
+        <location line="+19"/>
+        <location line="+97"/>
+        <location line="+35"/>
+        <location line="+32"/>
+        <location line="+40"/>
+        <location line="+26"/>
+        <location line="+13"/>
+        <location line="+13"/>
+        <location line="+13"/>
+        <location line="+26"/>
+        <location line="+119"/>
+        <location line="+26"/>
+        <location line="+26"/>
+        <source> mm</source>
+        <translation> мм</translation>
+    </message>
+    <message>
+        <location line="-498"/>
+        <location line="+116"/>
+        <source>Side</source>
+        <translation>Сбоку</translation>
+    </message>
+    <message>
+        <location line="-65"/>
+        <location line="+132"/>
+        <source>Front</source>
+        <translation>Спереди</translation>
+    </message>
+    <message>
+        <location line="-107"/>
+        <source>Cap</source>
+        <translation>Кепка</translation>
+    </message>
+    <message>
+        <location line="+135"/>
+        <source>Custom</source>
+        <translation>Свой</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <location line="+169"/>
+        <location line="+67"/>
+        <source>z:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="-210"/>
+        <location line="+104"/>
+        <location line="+132"/>
+        <source>x:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="-171"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Location of the two remaining model points&lt;br/&gt;with respect to the reference point in default pose&lt;/p&gt;&lt;p&gt;Use any units you want, not necessarily centimeters.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translatorcomment>Расположение двух оставшихся точек модели относительно опорной точки в стандартной позе. Возможно исп-ть любые единицы измерения, не обязательно сантиметры.</translatorcomment>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt; Расположение двух оставшихся точек модели&lt;br/&gt;относительно опорной точки в стандартной позе. &lt;/p&gt;&lt;p&gt;Возможно использовать любые единицы измерения.&lt;/p&gt;&lt;/body&gt;&lt;/html</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <location line="+65"/>
+        <location line="+106"/>
+        <source>y:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="-145"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:16pt;&quot;&gt;P&lt;/span&gt;&lt;span style=&quot; font-size:16pt; vertical-align:sub;&quot;&gt;3&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:16pt;&quot;&gt;P&lt;/span&gt;&lt;span style=&quot; font-size:16pt; vertical-align:sub;&quot;&gt;2&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+46"/>
+        <source>Model position</source>
+        <translation>Положение модели</translation>
+    </message>
+    <message>
+        <location line="+105"/>
+        <source>Use only yaw and pitch while calibrating.
+Don&apos;t roll or change position.</source>
+        <translation>Во время калибровки
+используйте только оси 
+YAW и PITCH.
+Не используйте оси 
+ROLL или X/Y-смещения.</translation>
+    </message>
+    <message>
+        <location line="+36"/>
+        <source>Start calibration</source>
+        <translation>Начать калибровку</translation>
+    </message>
+    <message>
+        <location line="+17"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;FTNoIR PointTracker Plugin&lt;br/&gt;Version 1.1&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;by Patrick Ruoff&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;http://ftnoirpt.sourceforge.net/&quot;&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline; color:#0000ff;&quot;&gt;Manual (external)&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;FTNoIR PointTracker Plugin&lt;br/&gt;Version 1.1&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;by Patrick Ruoff&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;http://ftnoirpt.sourceforge.net/&quot;&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline; color:#0000ff;&quot;&gt;Руководство (PointTracker)&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location line="+36"/>
+        <source>Status</source>
+        <translation>Статус</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Extracted Points:</source>
+        <translation>Извлечено точек:</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Camera Info:</source>
+        <translation>Параметры камеры:</translation>
+    </message>
+</context>
 </TS>
