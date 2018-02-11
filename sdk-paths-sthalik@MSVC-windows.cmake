31c4e60dc14bf343eb4be8802c65d4c02026d9aa
Stanislaw Halik <sthalik@misaki.pl>
2018-02-11 22:18:28 +0100

    cmake: change my defaults

--- sdk-paths-sthalik@MSVC-windows.cmake
+++ sdk-paths-sthalik@MSVC-windows.cmake
@@ -4,26 +4,29 @@
 # remember to change -MD to -MT in mkspecs/
 # also add CFLAGS -Zi and LFLAGS -DEBUG
 
+function(setq name value)
+    set("${name}" "${value}" CACHE INTERNAL "" FORCE)
+endfunction()
+
 set(Qt5_DIR "D:/dev/qt-5.10.0/lib/cmake/Qt5")
 set(__depdir "d:/dev/opentrack-depends/")
 set(OpenCV_DIR "${__depdir}/opencv/build")
 
-set(opentrack_disable-i18n-update TRUE CACHE BOOL "" FORCE)
-set(opentrack_install-debug-info TRUE CACHE BOOL "" FORCE)
+setq(opentrack_install-debug-info TRUE)
 
-set(SDK_ARUCO_LIBPATH "${__depdir}/aruco/build/src/aruco.lib" CACHE FILEPATH "" FORCE)
-set(EIGEN3_INCLUDE_DIR "${__depdir}/eigen" CACHE PATH "" FORCE)
+setq(SDK_ARUCO_LIBPATH "${__depdir}/aruco/build/src/aruco.lib")
+setq(EIGEN3_INCLUDE_DIR "${__depdir}/eigen")
 
-set(SDK_FSUIPC "${__depdir}/fsuipc" CACHE PATH "" FORCE)
-set(SDK_HYDRA "${__depdir}/SixenseSDK" CACHE PATH "" FORCE)
+setq(SDK_FSUIPC "${__depdir}/fsuipc")
+setq(SDK_HYDRA "${__depdir}/SixenseSDK")
 
-set(SDK_RIFT_025 "${__depdir}/LibOVR-025/build" CACHE PATH "" FORCE)
-set(SDK_RIFT_042 "${__depdir}/LibOVR-042/build" CACHE PATH "" FORCE)
-set(SDK_RIFT_080 "${__depdir}/LibOVR-080/build" CACHE PATH "" FORCE)
-set(SDK_RIFT_140 "${__depdir}/LibOVR-140/build" CACHE PATH "" FORCE)
+setq(SDK_RIFT_025 "${__depdir}/LibOVR-025/build")
+setq(SDK_RIFT_042 "${__depdir}/LibOVR-042/build")
+setq(SDK_RIFT_080 "${__depdir}/LibOVR-080/build")
+setq(SDK_RIFT_140 "${__depdir}/LibOVR-140/build")
 
-set(SDK_VALVE_STEAMVR "${__depdir}/steamvr" CACHE PATH "" FORCE)
-set(SDK_TOBII_EYEX "${__depdir}/Tobii-EyeX" CACHE PATH "" FORCE)
-set(SDK_VJOYSTICK "${__depdir}/vjoystick" CACHE PATH "" FORCE)
+setq(SDK_VALVE_STEAMVR "${__depdir}/steamvr")
+setq(SDK_TOBII_EYEX "${__depdir}/Tobii-EyeX")
+setq(SDK_VJOYSTICK "${__depdir}/vjoystick")
 
-set(SDK_REALSENSE "D:/RSSDK-R2" CACHE PATH "" FORCE)
+setq(SDK_REALSENSE "D:/RSSDK-R2")
