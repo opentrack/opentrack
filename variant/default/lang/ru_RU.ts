65666424847bc9d3c46af82d8cd1237398a4c607
Stanislaw Halik <sthalik@misaki.pl>
2018-02-16 20:57:47 +0100

    opentrack: fix ru_RU after move to variant/default

--- variant/default/lang/ru_RU.ts
+++ variant/default/lang/ru_RU.ts
@@ -5,86 +5,154 @@
     <name>UI_new_config</name>
     <message>
         <source>Config filename</source>
-        <translation type="unfinished"></translation>
+        <translation>Создание профиля</translation>
     </message>
     <message>
         <source>New file name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Новое имя профиля:</translation>
     </message>
 </context>
 <context>
     <name>main_window</name>
     <message>
-        <source>Raw tracker data</source>
-        <translation type="unfinished"></translation>
+        <source>Create new empty config</source>
+        <translation>Создать новый пустой профиль</translation>
     </message>
     <message>
-        <source>Z</source>
-        <translation type="unfinished"></translation>
+        <source>Create new copied config</source>
+        <translation>Создать новый профиль на основе текущего</translation>
     </message>
     <message>
-        <source>Pitch</source>
-        <translation type="unfinished"></translation>
+        <source>Open configuration directory</source>
+        <translation>Открыть каталог с профилями</translation>
     </message>
     <message>
-        <source>Y</source>
-        <translation type="unfinished"></translation>
+        <source>opentrack</source>
+        <translation></translation>
     </message>
     <message>
-        <source>X</source>
-        <translation type="unfinished"></translation>
+        <source> (debug)</source>
+        <translation></translation>
     </message>
     <message>
-        <source>Roll</source>
-        <translation type="unfinished"></translation>
+        <source>Show the Octopus</source>
+        <translation>Показать осьминожка</translation>
     </message>
     <message>
-        <source>Yaw</source>
-        <translation type="unfinished"></translation>
+        <source>Hide the Octopus</source>
+        <translation>Спрятать осьминожка</translation>
     </message>
     <message>
-        <source>Game data</source>
-        <translation type="unfinished"></translation>
+        <source>Tracker settings</source>
+        <translation>Настройка источника данных</translation>
     </message>
     <message>
-        <source>Profile</source>
-        <translation type="unfinished"></translation>
+        <source>Filter settings</source>
+        <translation>Настройка фильтрации/сглаживания</translation>
+    </message>
+    <message>
+        <source>Protocol settings</source>
+        <translation>Настройка выходного интерфейса</translation>
+    </message>
+    <message>
+        <source>Mappings</source>
+        <translation>Настройка кривых</translation>
     </message>
     <message>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <source>The Octopus is sad</source>
+        <translation>Осьминожек опечален</translation>
+    </message>
+    <message>
+        <source>Check permissions for your .ini directory:
+
+&quot;%1&quot;%2
+
+Exiting now.</source>
+        <translation>Проверьте права доступа на Вашу .ini папку:
+
+&quot;%1&quot;%2
+
+Закрытие программы.</translation>
+    </message>
+    <message>
+        <source> :: </source>
+        <translation> :: </translation>
+    </message>
+    <message>
+        <source>Raw tracker data</source>
+        <translation>Исходные данные</translation>
+    </message>
+    <message>
+        <source>Z</source>
+        <translation>Z</translation>
+    </message>
+    <message>
+        <source>Pitch</source>
+        <translation>Pitch</translation>
+    </message>
+    <message>
+        <source>Y</source>
+        <translation>Y</translation>
+    </message>
+    <message>
+        <source>X</source>
+        <translation>X</translation>
+    </message>
+    <message>
+        <source>Roll</source>
+        <translation>Roll</translation>
+    </message>
+    <message>
+        <source>Yaw</source>
+        <translation>Yaw</translation>
+    </message>
+    <message>
+        <source>Game data</source>
+        <translation>Игровые данные</translation>
+    </message>
+    <message>
+        <source>Profile</source>
+        <translation>Профиль</translation>
     </message>
     <message>
         <source>Mapping</source>
-        <translation type="unfinished"></translation>
+        <translation> Кривые</translation>
     </message>
     <message>
         <source>Tracking</source>
-        <translation type="unfinished"></translation>
+        <translation>Статус работы трекера</translation>
     </message>
     <message>
         <source>Start</source>
-        <translation type="unfinished"></translation>
+        <translation>Запустить</translation>
     </message>
     <message>
         <source>Stop</source>
-        <translation type="unfinished"></translation>
+        <translation>Остановить</translation>
     </message>
     <message>
         <source>Input</source>
-        <translation type="unfinished"></translation>
+        <translation>Источник данных</translation>
     </message>
     <message>
         <source>🔨</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Output</source>
-        <translation type="unfinished"></translation>
+        <translation>Выходной интерфейс</translation>
     </message>
     <message>
         <source>Filter</source>
-        <translation type="unfinished"></translation>
+        <translation>Фильтрация/сглаживание</translation>
     </message>
     <message>
         <source>Running as root is bad</source>
@@ -110,80 +178,16 @@
         <source>Do not run as root. Be annoyed, comprehensively.</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>Create new empty config</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Create new copied config</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Open configuration directory</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>opentrack</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source> (debug)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Show the Octopus</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Hide the Octopus</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Tracker settings</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Filter settings</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Protocol settings</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Mappings</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Exit</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>The Octopus is sad</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Check permissions for your .ini directory:
-
-&quot;%1&quot;%2
-
-Exiting now.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source> :: </source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>new_file_dialog</name>
     <message>
         <source>File exists</source>
-        <translation type="unfinished"></translation>
+        <translation>Файл создан</translation>
     </message>
     <message>
         <source>This file already exists. Pick another name.</source>
-        <translation type="unfinished"></translation>
+        <translation>Данный файл уже создан. Пожалуйста выберите другое имя.</translation>
     </message>
 </context>
 </TS>
