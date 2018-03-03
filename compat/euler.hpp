3da5f01888cd1c7e8a646895b7633e1d0c326e4e
Stanislaw Halik <sthalik@misaki.pl>
2018-03-03 12:58:02 +0100

    compat/euler: remove unused functions

--- compat/euler.hpp
+++ compat/euler.hpp
@@ -14,22 +14,12 @@ namespace euler {
 
 template<int h_, int w_> using dmat = Mat<double, h_, w_>;
 
-using dvec2 = Mat<double, 2, 1>;
 using dvec3 = Mat<double, 3, 1>;
 
 using rmat = dmat<3, 3>;
 using euler_t = dmat<3, 1>;
 
 rmat OTR_COMPAT_EXPORT euler_to_rmat(const euler_t& input);
-
 euler_t OTR_COMPAT_EXPORT rmat_to_euler(const rmat& R);
-
-void OTR_COMPAT_EXPORT tait_bryan_to_matrices(const euler_t& input,
-                                                   rmat& r_roll,
-                                                   rmat& r_pitch,
-                                                   rmat& r_yaw);
-
-rmat OTR_COMPAT_EXPORT quaternion_to_mat(const dmat<1, 4>& q);
-//rmat OTR_COMPAT_EXPORT quaternion_to_mat(const dmat<4, 1>& q);
 
 } // end ns euler
