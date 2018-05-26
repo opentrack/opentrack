1fb5c05624152a4e082b1cb167c3a8418486a864
Stanislaw Halik <sthalik@misaki.pl>
2018-05-26 21:15:28 +0200

    cv/video-property-page: fix qv4l2 invocation for Linux #726
    Submitted-by: Artem Sobolev

--- cv/video-property-page.cpp
+++ cv/video-property-page.cpp
@@ -153,7 +153,7 @@ bool video_property_page::show(int idx)
     const QList<QString> camera_names(get_camera_names());
 
     if (idx >= 0 && idx < camera_names.size())
-        return QProcess::startDetached("qv4l2", QStringList() << "-d" << camera_names[idx]);
+        return QProcess::startDetached("qv4l2", QStringList { "-d", QString("/dev/video%1").arg(idx) });
     else
         return false;
 }
