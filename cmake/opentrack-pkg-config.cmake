5a5b02987823f9c7b8bcd832f9acf8bcaff73cc0
Stanislaw Halik <sthalik@misaki.pl>
2018-05-27 22:39:44 +0200

    cmake/pkg-config: space _after_ append_string text
    Keeps it in line with the rest of the APPEND_STRING code. Putting the
    space inconsistently makes the whole thing blow up.

--- cmake/opentrack-pkg-config.cmake
+++ cmake/opentrack-pkg-config.cmake
@@ -16,9 +16,9 @@ function(otr_pkgconfig target)
         endif()
     endforeach()
     #message(STATUS "foo | ${cflags} | ${includes} | ${ldflags}")
-    set_property(TARGET ${target} APPEND_STRING PROPERTY COMPILE_FLAGS " ${cflags}")
+    set_property(TARGET ${target} APPEND_STRING PROPERTY COMPILE_FLAGS "${cflags} ")
     target_include_directories(${target} SYSTEM PRIVATE ${includes})
-    set_property(TARGET ${target} APPEND_STRING PROPERTY LINK_FLAGS " ${ldflags}")
+    set_property(TARGET ${target} APPEND_STRING PROPERTY LINK_FLAGS "${ldflags} ")
 endfunction()
 
 
