4472ef37e5725da8331fc0b2ca4f436cbe73fd76
Stanislaw Halik <sthalik@misaki.pl>
2018-05-27 05:37:23 +0200

    gui: appease linter

--- gui/init.cpp
+++ gui/init.cpp
@@ -183,7 +183,7 @@ int run_window(QApplication& app, std::unique_ptr<QWidget> main_window)
     return status;
 }
 
-int otr_main(int argc, char** argv, std::function<QWidget*()> make_main_window)
+int otr_main(int argc, char** argv, std::function<QWidget*()> const& make_main_window)
 {
 #if defined OTR_HAS_DENORM_CONTROL
     set_fp_mask();
