392c0b904a56ea84f2069080ff1e3303cc775e00
Stanislaw Halik <sthalik@misaki.pl>
2018-05-27 06:21:46 +0200

    proto/wine: update translation

--- proto-wine/lang/stub.ts
+++ proto-wine/lang/stub.ts
@@ -4,23 +4,33 @@
 <context>
     <name>UICFTControls</name>
     <message>
-        <location filename="../ftnoir_winecontrols.ui" line="+17"/>
         <source>FreeTrack settings FaceTrackNoIR</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location line="+48"/>
         <source>There are no settings necessary for the Wine protocol.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location line="+47"/>
         <source>OK</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location line="+25"/>
         <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>wine</name>
+    <message>
+        <source>Can&apos;t open shared memory mapping</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>wineDll</name>
+    <message>
+        <source>Wine -- Windows layer for Unix</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
