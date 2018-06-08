8a989904c8d8199a94aaaec5c415badd411f4b80
Wei Shuai <cpuwolf@gmail.com>
2018-06-08 15:49:27 +0800

    gui: update Chinese translation

--- gui/lang/zh_CN.ts
+++ gui/lang/zh_CN.ts
@@ -5,11 +5,11 @@
     <name>BrowseButton</name>
     <message>
         <source>Set executable name</source>
-        <translation type="unfinished"></translation>
+        <translation>选择可执行文件</translation>
     </message>
     <message>
         <source>Executable (*.exe);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>可执行文件 (*.exe);;所有文件 (*)</translation>
     </message>
 </context>
 <context>
@@ -17,371 +17,372 @@
     <message>
         <source>Press &quot;calibrate&quot; in given row to calibrate that axis. Follow instructions in the next window.
 Press &quot;clear calibration&quot; to remove any calibration data pertaining to that axis of position change or rotation.</source>
-        <translation type="unfinished"></translation>
+        <translation>按 &quot;校准&quot; in given row to calibrate that axis. 按照下一个窗口的指示继续.
+按 &quot;清除校准&quot; to remove any calibration data pertaining to that axis of position change or rotation.</translation>
     </message>
     <message>
         <source>X</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Y</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Z</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Yaw</source>
-        <translation type="unfinished"></translation>
+        <translation>航向</translation>
     </message>
     <message>
         <source>Pitch</source>
-        <translation type="unfinished"></translation>
+        <translation>仰俯</translation>
     </message>
     <message>
         <source>Roll</source>
-        <translation type="unfinished"></translation>
+        <translation>横滚</translation>
     </message>
     <message>
         <source>Calibrate</source>
-        <translation type="unfinished"></translation>
+        <translation>校准</translation>
     </message>
     <message>
         <source>Clear calibration</source>
-        <translation type="unfinished"></translation>
+        <translation>清除校准</translation>
     </message>
     <message>
         <source>1</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>keyboard_listener</name>
     <message>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>按键绑定对话框</translation>
     </message>
     <message>
         <source>Press a key or close this window to remove the keybinding.</source>
-        <translation type="unfinished"></translation>
+        <translation>按键绑定，或者关闭窗口消除绑定。</translation>
     </message>
 </context>
 <context>
     <name>mapping_dialog</name>
     <message>
         <source>Mapping properties</source>
-        <translation type="unfinished"></translation>
+        <translation>映射属性</translation>
     </message>
     <message>
         <source>Yaw</source>
-        <translation type="unfinished"></translation>
+        <translation>航向</translation>
     </message>
     <message>
         <source>Max input</source>
-        <translation type="unfinished"></translation>
+        <translation>最大输入</translation>
     </message>
     <message>
         <source>Asymmetric mapping below</source>
-        <translation type="unfinished"></translation>
+        <translation>非对称映射</translation>
     </message>
     <message>
         <source>Pitch</source>
-        <translation type="unfinished"></translation>
+        <translation>仰俯</translation>
     </message>
     <message>
         <source>Max output</source>
-        <translation type="unfinished"></translation>
+        <translation>最大输出</translation>
     </message>
     <message>
         <source>180°</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>90°</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Roll</source>
-        <translation type="unfinished"></translation>
+        <translation>横滚</translation>
     </message>
     <message>
         <source>X</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Y</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Z</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>%1°</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>options_dialog</name>
     <message>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>选项</translation>
     </message>
     <message>
         <source>Shortcuts</source>
-        <translation type="unfinished"></translation>
+        <translation>快捷键</translation>
     </message>
     <message>
         <source>Global shortcuts</source>
-        <translation type="unfinished"></translation>
+        <translation>全局快捷键</translation>
     </message>
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Center&lt;/span&gt; - use current pose as looking perfectly forward.&lt;br/&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Toggle&lt;/span&gt; - keep looking at same spot until next toggle keypress.&lt;br/&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Zero&lt;/span&gt; - keep looking forward until next zero keypress.&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;回中位置&lt;/span&gt; - 以现在的姿势为最完美向前看.&lt;br/&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;盯着看&lt;/span&gt; - 盯着固定位置直到再次按键.&lt;br/&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;归零位置&lt;/span&gt; - 保持向前看直到再次按键.&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <source>Center</source>
-        <translation type="unfinished"></translation>
+        <translation>回中位置</translation>
     </message>
     <message>
         <source>Bind</source>
-        <translation type="unfinished"></translation>
+        <translation>绑定</translation>
     </message>
     <message>
         <source>Start tracking</source>
-        <translation type="unfinished">开始跟踪</translation>
+        <translation>开始跟踪</translation>
     </message>
     <message>
         <source>Stop tracking</source>
-        <translation type="unfinished">停止跟踪</translation>
+        <translation>停止跟踪</translation>
     </message>
     <message>
         <source>Zero</source>
-        <translation>初始位置</translation>
+        <translation>归零位置</translation>
     </message>
     <message>
         <source>Toggle</source>
-        <translation type="unfinished"></translation>
+        <translation>盯着看</translation>
     </message>
     <message>
         <source>Zero while held</source>
-        <translation type="unfinished"></translation>
+        <translation>长按归零</translation>
     </message>
     <message>
         <source>Restart tracking</source>
-        <translation type="unfinished"></translation>
+        <translation>重新开始跟踪</translation>
     </message>
     <message>
         <source>Toggle while held</source>
-        <translation type="unfinished"></translation>
+        <translation>长按盯着看</translation>
     </message>
     <message>
         <source>Toggle tracking</source>
-        <translation type="unfinished"></translation>
+        <translation>开关跟踪</translation>
     </message>
     <message>
         <source>Center at startup</source>
-        <translation type="unfinished"></translation>
+        <translation>启动时自动回中</translation>
     </message>
     <message>
         <source>Never translate the application interface</source>
-        <translation type="unfinished"></translation>
+        <translation>关闭翻译界面</translation>
     </message>
     <message>
         <source>Minimize to tray</source>
-        <translation type="unfinished"></translation>
+        <translation>最小化到任务栏</translation>
     </message>
     <message>
         <source>Enable tray</source>
-        <translation type="unfinished"></translation>
+        <translation>开启任务栏图标</translation>
     </message>
     <message>
         <source>Minimize to tray on startup when enabled</source>
-        <translation type="unfinished"></translation>
+        <translation>启动时最小化到任务栏</translation>
     </message>
     <message>
         <source>Output</source>
-        <translation type="unfinished"></translation>
+        <translation>输出</translation>
     </message>
     <message>
         <source>Output remap</source>
-        <translation type="unfinished"></translation>
+        <translation>输出重新映射</translation>
     </message>
     <message>
         <source>X</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Y</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Z</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Yaw</source>
-        <translation type="unfinished"></translation>
+        <translation>航向</translation>
     </message>
     <message>
         <source>Pitch</source>
-        <translation type="unfinished"></translation>
+        <translation>仰俯</translation>
     </message>
     <message>
         <source>Roll</source>
-        <translation type="unfinished"></translation>
+        <translation>横滚</translation>
     </message>
     <message>
         <source>Disabled</source>
-        <translation type="unfinished"></translation>
+        <translation>不启用</translation>
     </message>
     <message>
         <source>Relative translation only</source>
-        <translation type="unfinished"></translation>
+        <translation>相对位移</translation>
     </message>
     <message>
         <source>Source</source>
-        <translation type="unfinished"></translation>
+        <translation>源头</translation>
     </message>
     <message>
         <source>Invert</source>
-        <translation type="unfinished"></translation>
+        <translation>反向</translation>
     </message>
     <message>
         <source>Destination</source>
-        <translation type="unfinished"></translation>
+        <translation>目标</translation>
     </message>
     <message>
         <source>Assign input axis to output axis.</source>
-        <translation type="unfinished"></translation>
+        <translation>指定输入坐标到输出坐标.</translation>
     </message>
     <message>
         <source>Custom center pose</source>
-        <translation type="unfinished"></translation>
+        <translation>定制回中姿态</translation>
     </message>
     <message>
         <source>Alter the centered position sent to games. Useful if the default position is too much downward or upward.</source>
-        <translation type="unfinished"></translation>
+        <translation>改变游戏中的得到的回中位置.  对于默认位置有明显偏移的很有用.</translation>
     </message>
     <message>
         <source>°</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source> cm</source>
-        <translation type="unfinished"></translation>
+        <translation> 厘米</translation>
     </message>
     <message>
         <source>CSV Data Logging</source>
-        <translation type="unfinished"></translation>
+        <translation>CSV数据记录</translation>
     </message>
     <message>
         <source>Enable - You will be asked for a filename whenever tracking starts</source>
-        <translation type="unfinished"></translation>
+        <translation>启用 - 当跟踪开始时会被要求输入文件名</translation>
     </message>
     <message>
         <source>Relative translation</source>
-        <translation type="unfinished"></translation>
+        <translation>相对位移</translation>
     </message>
     <message>
         <source>With relative mode on, translation is applied after rotation. For example, rotating +180 degrees yaw and moving backwards results in moving forward as a result of that rotation.</source>
-        <translation type="unfinished"></translation>
+        <translation>相对位移开启时，位移会被后考虑.例如,航向旋转+180度,然后后退的结果是,直接向前移动.</translation>
     </message>
     <message>
         <source>Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>模式</translation>
     </message>
     <message>
         <source>Enabled</source>
-        <translation type="unfinished"></translation>
+        <translation>启用</translation>
     </message>
     <message>
         <source>Enabled when not aiming</source>
-        <translation type="unfinished"></translation>
+        <translation>没有朝向它时启用</translation>
     </message>
     <message>
         <source>Disable for Y</source>
-        <translation type="unfinished"></translation>
+        <translation>Y轴不启用</translation>
     </message>
     <message>
         <source>Disable for X</source>
-        <translation type="unfinished"></translation>
+        <translation>X轴不启用</translation>
     </message>
     <message>
         <source>Disable effect by roll</source>
-        <translation type="unfinished"></translation>
+        <translation>横滚时，不启用</translation>
     </message>
     <message>
         <source>Disable for Z (for zoom on Z axis)</source>
-        <translation type="unfinished"></translation>
+        <translation>Z轴不启用(用于Z轴放大)</translation>
     </message>
     <message>
         <source>Disable effect by pitch</source>
-        <translation type="unfinished"></translation>
+        <translation>仰俯不启用</translation>
     </message>
     <message>
         <source>Disable effect by yaw</source>
-        <translation type="unfinished"></translation>
+        <translation>航向不启用</translation>
     </message>
     <message>
         <source>Neck displacement</source>
-        <translation type="unfinished"></translation>
+        <translation>脖子的偏移</translation>
     </message>
     <message>
         <source>Eyes will be offset from the pivot of rotation, assumed to be the neck. It also works with relative translation disabled.</source>
-        <translation type="unfinished"></translation>
+        <translation>眼睛的移动支点其实是脖子。本功能在相对位移功能不开启的情况下也生效。</translation>
     </message>
     <message>
         <source>Enable</source>
-        <translation type="unfinished"></translation>
+        <translation>启用</translation>
     </message>
     <message>
         <source>Forward from center of rotation</source>
-        <translation type="unfinished"></translation>
+        <translation>向前偏移</translation>
     </message>
     <message>
         <source>Game detection</source>
-        <translation type="unfinished"></translation>
+        <translation>游戏侦测</translation>
     </message>
     <message>
         <source>Start tracking automatically when a game starts with selected profile, and stop when the game exits.</source>
-        <translation type="unfinished"></translation>
+        <translation>当游戏开始运行时自动开始跟踪，游戏退出时停止跟踪。</translation>
     </message>
     <message>
         <source>Joy button %1</source>
-        <translation type="unfinished"></translation>
+        <translation>游戏手柄按钮 %1</translation>
     </message>
     <message>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>空</translation>
     </message>
 </context>
 <context>
     <name>process_detector</name>
     <message>
         <source>Game detector</source>
-        <translation type="unfinished"></translation>
+        <translation>游戏侦测</translation>
     </message>
     <message>
         <source>Start profiles from game executable names in this list</source>
-        <translation type="unfinished"></translation>
+        <translation>运行以下列表可执行文件时启用对应的配置文件</translation>
     </message>
     <message>
         <source>Executable</source>
-        <translation type="unfinished"></translation>
+        <translation>可执行文件</translation>
     </message>
     <message>
         <source>Profile</source>
-        <translation type="unfinished"></translation>
+        <translation>配置</translation>
     </message>
     <message>
         <source>+</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>-</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
