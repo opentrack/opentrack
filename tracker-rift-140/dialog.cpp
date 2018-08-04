299fb737fe711d00654ec5afe4cc2afdc742f279
Stanislaw Halik <sthalik@misaki.pl>
2018-08-04 15:16:49 +0200

    tracker/rift: fix build

--- tracker-rift-140/dialog.cpp
+++ tracker-rift-140/dialog.cpp
@@ -3,15 +3,10 @@
 
 dialog_rift_140::dialog_rift_140()
 {
-    ui.setupUi( this );
+    ui.setupUi(this);
 
     connect(ui.buttonBox, SIGNAL(accepted()), this, SLOT(doOK()));
     connect(ui.buttonBox, SIGNAL(rejected()), this, SLOT(doCancel()));
-
-    tie_setting(s.constant_drift, ui.constantDrift);
-    tie_setting(s.deadzone, ui.deadzone);
-    tie_setting(s.persistence, ui.persistence);
-    tie_setting(s.useYawSpring, ui.yawSpring);
 }
 
 void dialog_rift_140::doOK() {
