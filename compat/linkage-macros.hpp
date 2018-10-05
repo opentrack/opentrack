d908b5a684a7183e125e84873b1f13aaec804287
Stanislaw Halik <sthalik@misaki.pl>
2018-10-05 16:30:22 +0200

    compat/linkage-macros: fix template exports on mingw-w64

--- compat/linkage-macros.hpp
+++ compat/linkage-macros.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
-#if defined _WIN32 && !defined __WINE__
+#if defined _MSC_VER
 #   define OTR_GENERIC_EXPORT __declspec(dllexport)
 #   define OTR_GENERIC_IMPORT __declspec(dllimport)
+#elif defined _WIN32 && !defined __WINE__
+#   define OTR_GENERIC_EXPORT __attribute__((dllexport, visibility ("default")))
+#   define OTR_GENERIC_IMPORT __attribute__((dllimport, visibility ("default")))
 #else
 #   define OTR_GENERIC_EXPORT __attribute__ ((visibility ("default")))
 #   define OTR_GENERIC_IMPORT
 #endif
 
-#if defined __MINGW32__ || defined __APPLE__
+#if defined __APPLE__
 #   define OTR_TEMPLATE_IMPORT(x) //nothing
 #else
 #   define OTR_TEMPLATE_IMPORT(x) extern template class OTR_GENERIC_IMPORT x
@@ -16,7 +19,7 @@
 
 #define OTR_TEMPLATE_EXPORT_(x) template class OTR_GENERIC_EXPORT x
 
-#if defined __MINGW32__ || defined __APPLE__
+#if defined __APPLE__
 #   define OTR_TEMPLATE_EXPORT(x) // nothing
 #else /* does this _always_ work for binutils ELF? */
 #   define OTR_TEMPLATE_EXPORT(x) OTR_TEMPLATE_EXPORT_(x)
