1b1e0f4ef7ae31e2721e6a32a84548e66f4244ef
Stanislaw Halik <sthalik@misaki.pl>
2018-12-20 20:26:44 +0100

    compat/macros: split off the real macro-only file

--- compat/macros.hpp
+++ compat/macros.hpp
@@ -1,46 +1,8 @@
 #pragma once
 
-#if defined _MSC_VER
-#   define cc_noinline __declspec(noinline)
-#else
-#   define cc_noinline __attribute__((noinline))
-#endif
+#include "macros1.h"
 
-#if defined _MSC_VER
-#   define cc_forceinline __forceinline
-#else
-#   define cc_forceinline __attribute__((always_inline))
-#endif
-
-#if !defined likely
-#   if defined __GNUC__
-#      define likely(x)       __builtin_expect(!!(x),1)
-#      define unlikely(x)     __builtin_expect(!!(x),0)
-#   else
-#      define likely(x) (x)
-#      define unlikely(x) (x)
-#   endif
-#endif
-
-#if defined _MSC_VER
-#   define cc_function_name __FUNCSIG__
-#else
-#   define cc_function_name __PRETTY_FUNCTION__
-#endif
-
-#if !defined PP_CAT
-#   define PP_CAT(x,y) PP_CAT1(x,y)
-#   define PP_CAT1(x,y) PP_CAT2(x,y)
-#   define PP_CAT2(x,y) x ## y
-#endif
-
-#ifndef PP_EXPAND
-#   define PP_EXPAND(x) PP_EXPAND__2(x)
-#   define PP_EXPAND__2(x) PP_EXPAND__3(x) x
-#   define PP_EXPAND__3(x) x
-#endif
-
-#if defined __cplusplus
+#ifdef __cplusplus
 
 // from now only C++
 
