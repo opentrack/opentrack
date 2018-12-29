9fd246a411e9faaf49c6b18c020fde351b9412a6
Stanislaw Halik <sthalik@misaki.pl>
2018-12-29 13:31:00 +0100

    compat/enum-operators: add assign operators

--- compat/enum-operators.hpp
+++ compat/enum-operators.hpp
@@ -20,10 +20,26 @@
         return type(op t_((x)));                                        \
     } // end
 
+#define OTR_FLAGS_ASSIGN_OP(type, op)                                   \
+    inline type& operator op ## = (type& lhs, type rhs)                 \
+    {                                                                   \
+        using t_ = std::underlying_type_t<decltype(rhs)>;               \
+        lhs = type(t_((lhs)) op t_((rhs)));                             \
+        return lhs;                                                     \
+    } //end
+
+#define OTR_FLAGS_DELETE_SHIFT_ASSIGN(type, op)                         \
+    type operator op ## = (type& lhs, type rhs) = delete //end
+
 #define DEFINE_ENUM_OPERATORS(type)                                     \
     OTR_FLAGS_OP2(type, |)                                              \
     OTR_FLAGS_OP2(type, &)                                              \
     OTR_FLAGS_OP2(type, ^)                                              \
     OTR_FLAGS_OP1(type, ~)                                              \
-    OTR_FLAGS_SHIFT(type, <<);                                          \
-    OTR_FLAGS_SHIFT(type, >>) // end
+    OTR_FLAGS_DELETE_SHIFT(type, <<);                                   \
+    OTR_FLAGS_DELETE_SHIFT(type, >>);                                   \
+    OTR_FLAGS_ASSIGN_OP(type, |)                                        \
+    OTR_FLAGS_ASSIGN_OP(type, &)                                        \
+    OTR_FLAGS_ASSIGN_OP(type, ^)                                        \
+    OTR_FLAGS_DELETE_SHIFT_ASSIGN(type, <<);                            \
+    OTR_FLAGS_DELETE_SHIFT_ASSIGN(type, >>) // end
