47d395c98e2d11840f775e22386167f27c199e1c
Stanislaw Halik <sthalik@misaki.pl>
2019-01-03 00:10:05 +0100

    tracker/wii: don't export more than necessary

--- tracker-wii/wii_module.hpp
+++ tracker-wii/wii_module.hpp
@@ -2,10 +2,7 @@
 
 #include "api/plugin-api.hpp"
 
-#include <QIcon>
-#include <QtGlobal>
-
-class Q_DECL_EXPORT wii_metadata_pt : public Metadata
+class wii_metadata_pt : public Metadata
 {
     Q_OBJECT
 
