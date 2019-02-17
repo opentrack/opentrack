7f2103eb4c91b4c1801ddfefeda5ac8d8b7c49d8
Stéphane Lenclud <github@lenclud.com>
2019-02-17 10:56:24 +0100

    Kinect: Copyright fix and minor cmake changes.

--- tracker-kinect-face/kinect_face_tracker.cpp
+++ tracker-kinect-face/kinect_face_tracker.cpp
@@ -1,4 +1,10 @@
+/* Copyright (c) 2019, Stéphane Lenclud <github@lenclud.com>
 
+ * Permission to use, copy, modify, and/or distribute this
+ * software for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice and this permission
+ * notice appear in all copies.
+ */
 
 #include "kinect_face_tracker.h"
 
