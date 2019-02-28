55afd194527e3c40835d32f872d3c379ad3279c4
Stanislaw Halik <sthalik@misaki.pl>
2019-02-28 19:57:43 +0100

    compat: don't pull in macros header pointlessly

--- compat/base-path.hpp
+++ compat/base-path.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#include "macros.hpp"
 #include "export.hpp"
 
 #include <QString>
 
-OTR_COMPAT_EXPORT never_inline
+OTR_COMPAT_EXPORT
 const QString& application_base_path();
 
 #define OPENTRACK_BASE_PATH (application_base_path())
+
