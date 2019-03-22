9036671d22dd1d5b77c17055bc2d87ed73ea83df
Stéphane Lenclud <github@lenclud.com>
2019-03-22 10:51:16 +0100

    Fixing copyrights

--- tracker-kinect-face/camera_kinect_ir.h
+++ tracker-kinect-face/camera_kinect_ir.h
@@ -1,8 +1,9 @@
-/* Copyright (c) 2019 Stéphane Lenclud
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
+/* Copyright (c) 2019, Stephane Lenclud <github@lenclud.com>
+
+ * Permission to use, copy, modify, and/or distribute this
+ * software for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice and this permission
+ * notice appear in all copies.
  */
 
 #pragma once
