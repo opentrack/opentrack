753013079c2e2d2db82dd096c642c5140f1de772
Stanislaw Halik <sthalik@misaki.pl>
2019-03-28 08:35:21 +0100

    compat/tr: make header-only for the api header

--- compat/tr.hpp
+++ compat/tr.hpp
@@ -1,18 +1,14 @@
 #pragma once
 
 #include <QObject>
-#include "export.hpp"
 
 // The class does nothing except provide a fake assignment operator for QObject
 // It's meant to be used inside classes that need i18n support but are returned by value.
 
-class OTR_COMPAT_EXPORT TR : public QObject
+struct TR : QObject
 {
-    Q_OBJECT
+    TR() = default;
+    TR(const TR&) {}
 
-public:
-    TR();
-    TR(const TR&);
-
-    TR& operator=(const TR& other);
+    TR& operator=(const TR&) { return *this; }
 };
