7e21dd631a766155ee4d6a5b24fb05dd691cf39f
Stéphane Lenclud <github@lenclud.com>
2019-04-12 20:46:20 +0200

    Kinect Tracker settings now showing telemetry notice as per EULA. Closes #929

--- tracker-kinect-face/lang/nl_NL.ts
+++ tracker-kinect-face/lang/nl_NL.ts
@@ -15,7 +15,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Start OpenTrack to check if Kinect Face Tracker is working.</source>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Start OpenTrack to check if Kinect Face Tracker is working.&lt;/p&gt;&lt;p&gt;&lt;b&gt;Note&lt;/b&gt;: When using OpenTrack with a Kinect for Windows v2 Sensor, Microsoft will collect telemetry data (e.g. operating system, number of processors, graphic chipset, memory, device type, locale, time) in order to improve Microsoft products and services. The data will not be used to identify specific individuals.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
