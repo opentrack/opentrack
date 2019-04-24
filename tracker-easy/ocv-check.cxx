ae76c229f0466034d64bcc1d3bab9dd47eb2f90a
Stanislaw Halik <sthalik@misaki.pl>
2019-04-24 20:43:44 +0200

    tracker/easy: add check for other modules

--- tracker-easy/ocv-check.cxx
+++ tracker-easy/ocv-check.cxx
@@ -1,7 +1,18 @@
 #include <opencv2/calib3d.hpp>
-
-static void check_solvep3p()
+void check_solvep3p()
 {
     cv::Mat x;
     cv::solveP3P(x, x, x, x, x, x, 0);
+}
+
+#include <opencv2/video/tracking.hpp>
+void check_kf()
+{
+    [[maybe_unused]] cv::KalmanFilter kf;
+}
+
+#include <opencv2/highgui.hpp>
+void check_highgui()
+{
+    cv::imshow("foo", {});
 }
