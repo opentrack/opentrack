02b80d8686fe393a3c69d4080873090e72b5cce5
Stanislaw Halik <sthalik@misaki.pl>
2019-05-02 18:26:01 +0200

    cmake: kill exception support for MSVC
    Issue: #940

--- cmake/opentrack-platform.cmake
+++ cmake/opentrack-platform.cmake
@@ -126,6 +126,7 @@ if(MSVC)
     add_definitions(-D_CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES=1)
     add_definitions(-D_SCL_SECURE_NO_WARNINGS)
 
+    add_compile_options(-EHs-c-)
     add_definitions(-D_HAS_EXCEPTIONS=0)
 
     add_definitions(-D_ENABLE_EXTENDED_ALIGNED_STORAGE)
