0cb91608deac61c0b2d8d76d7a24445b8e45928d
Stanislaw Halik <sthalik@misaki.pl>
2019-05-05 01:10:18 +0200

    cmake: update, sort my sdk paths

--- sdk-paths-sthalik@MSVC-windows.cmake
+++ sdk-paths-sthalik@MSVC-windows.cmake
@@ -21,21 +21,18 @@ set(opentrack_install-debug-info TRUE CACHE INTERNAL "" FORCE)
 set(opentrack_maintainer-mode TRUE CACHE INTERNAL "" FORCE)
 
 setq(Qt5_DIR "../qt-5.12.0/lib/cmake/Qt5")
-setq(OpenCV_DIR "opencv/build")
 
-setq(SDK_ARUCO_LIBPATH "aruco/build/src/aruco.lib")
 setq(EIGEN3_INCLUDE_DIR "eigen")
-
+setq(OpenCV_DIR "opencv/build")
+setq(SDK_ARUCO_LIBPATH "aruco/build/src/aruco.lib")
 setq(SDK_FSUIPC "fsuipc")
 setq(SDK_HYDRA "SixenseSDK")
-
+setq(SDK_KINECT20 "Kinect-v2.0")
+setq(SDK_PS3EYEDRIVER "PS3EYEDriver")
+setq(SDK_REALSENSE "RSSDK-R2")
 setq(SDK_RIFT_140 "LibOVR-140/build")
-
 setq(SDK_VALVE_STEAMVR "steamvr")
 setq(SDK_VJOYSTICK "vjoystick")
-
-setq(SDK_REALSENSE "RSSDK-R2")
-setq(SDK_KINECT20 "Kinect-v2.0")
 
 if(CMAKE_GENERATOR STREQUAL "NMake Makefiles")
     set(CMAKE_MAKE_PROGRAM "jom" CACHE STRING "" FORCE)
