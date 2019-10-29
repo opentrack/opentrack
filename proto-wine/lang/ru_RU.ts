5b0f05ec74fffb1380285ef491eb5b68aab386d6
Stanislaw Halik <sthalik@misaki.pl>
2019-10-29 04:03:49 +0100

    proto/wine: refresh i18n

--- proto-wine/lang/ru_RU.ts
+++ proto-wine/lang/ru_RU.ts
@@ -32,11 +32,11 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Application id (Proton only)</source>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;When supported.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;When supported.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <source>Steam application id (Proton)</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
