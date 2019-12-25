904ebd5a8ad3e2edc60db8852ef9a5af623321b1
R. van Twisk <ries.van.twisk@ing.com>
2019-12-25 14:06:05 +0100

    qhash fix for build on qt 5.14

--- compat/qhash.hpp
+++ compat/qhash.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
+#if (QT_VERSION < QT_VERSION_CHECK(5, 14, 0))
+#include <QHash>
 #include <QString>
-#include <QHashFunctions>
+#include <functional>
 
 namespace std {
-template<> struct hash<QString>
-{
-    unsigned operator()(const QString& value) const
-    {
-        return qHash(value);
+  template<> struct hash<QString> {
+    size_t operator()(const QString& s) const noexcept {
+      return (size_t) qHash(s);
     }
-};
+  };
 }
+#else
+#include <QHashFunctions>
+#endif
