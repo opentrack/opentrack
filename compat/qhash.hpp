28c06bef4392108fc219c0accefe5a7db7c9f065
Ries van Twisk <github@rvt.dds.nl>
2019-12-29 10:13:57 +0100

    Merge conflict fix

--- compat/qhash.hpp
+++ compat/qhash.hpp
@@ -1,17 +1,5 @@
 #pragma once
 
-#if (QT_VERSION < QT_VERSION_CHECK(5, 14, 0))
-#include <QHash>
-#include <QString>
 #include <functional>
-
-namespace std {
-  template<> struct hash<QString> {
-    size_t operator()(const QString& s) const noexcept {
-      return (size_t) qHash(s);
-    }
-  };
-}
-#else
+#include <QString>
 #include <QHashFunctions>
-#endif
