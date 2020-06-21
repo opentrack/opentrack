3c30242ee5f92ee5174cd2d91f3ee4f1e91d7785
GO63-samara <go1@list.ru>
2020-06-21 22:15:20 +0400

    Hamilton filter
    Add a new Hamilton filter.
    
    Hamilton Filter Key Features:
    - Instead of square, round (spherical) floating dead zones and smoothing areas are applied. Due to this, the angular size of these zones does not change when the Pitch angle changes. Diagonally rotations is as easy as moving along the Yaw and Pitch axes.
    - Rotations are not filtered by independent coordinates, but comprehensively, in 3D space. Rotations and movements are more natural. There are no view jumps at the borders of +/- 180 degrees.
    - The possibility of increasing the smoothing of rotations when zooming (when the head is approaching the monitor, that is, when increasing the -Z coordinate) is introduced. This makes it possible to more accurately aim and monitor remote targets.
    
    A full description of the Hamilton filter is available in Russian here:
    https://sites.google.com/site/diyheadtracking/home/opentrack/opentrack-hamilton-filter
    
    The Hamilton filter was tested by the Russian community, received positive reviews:
    https://forum.il2sturmovik.ru/topic/5061-opentrack-актуальная-информация-по-проекту-решение-проблем-вопросы/page/24/
    https://forums.eagle.ru/showthread.php?t=23280&page=249

---
+++ filter-hamilton/lang/stub.ts
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>UICdialog_hamilton</name>
+    <message>
+        <source>Rotations: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> Smoothing :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Positions: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> Max distance:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zoom smoothing:  </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hamilton filter settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Max Z:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>10,00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>0,01</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>2,00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>1,00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>0,02</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> Dead Zone:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> Smoothing:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>15,00</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>hamiltonDll</name>
+    <message>
+        <source>Hamilton</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
