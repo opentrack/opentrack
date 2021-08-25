d43a1446d9f4734c1b7b3e1dfd912eaf06cc0db1
Stanislaw Halik <sthalik@misaki.pl>
2021-08-25 06:19:14 +0200

    logic: fix pedantic warning

--- logic/main-settings.hpp
+++ logic/main-settings.hpp
@@ -73,7 +73,7 @@ struct OTR_LOGIC_EXPORT main_settings final
     value<bool> tray_start { b, "start-in-tray", false };
 
     value<bool> center_at_startup { b, "center-at-startup", true };
-    value<centering_state> centering_mode { b, "centering-mode", center_roll_compensated };;
+    value<centering_state> centering_mode { b, "centering-mode", center_roll_compensated };
     value<int> neck_z { b, "neck-depth", 0 };
     value<bool> neck_enable { b, "neck-enable", false };
 
