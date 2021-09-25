56766097bd5c90b564d833edc5c3c0e67be43dde
Stanislaw Halik <sthalik@misaki.pl>
2021-09-25 05:59:01 +0200

    compat/qt-dpi: get rid of mixin crap

--- compat/qt-dpi.hpp
+++ compat/qt-dpi.hpp
@@ -3,17 +3,22 @@
 #include <algorithm>
 #include <QWidget>
 
+static inline double screen_dpi(const QPaintDevice* widget)
+{
+    const auto& x = *widget;
+#ifdef _WIN32
+    return std::max(x.devicePixelRatioF(), 1.);
+#else
+    return std::max(std::max(x.logicalDpiX()/(double)x.physicalDpiX(), x.devicePixelRatioF()), 1.);
+#endif
+}
+
 template<typename self>
 struct screen_dpi_mixin
 {
 protected:
     double screen_dpi() const
     {
-        const auto& x = *static_cast<const self*>(this);
-#ifdef _WIN32
-        return std::max(x.devicePixelRatioF(), 1.);
-#else
-        return std::max(std::max(x.logicalDpiX()/(double)x.physicalDpiX(), x.devicePixelRatioF()), 1.);
-#endif
+        return ::screen_dpi(static_cast<const self*>(this));
     }
 };
