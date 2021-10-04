c2ab96e976ff12f1d5fdaefbb77042c024cb47dd
Stanislaw Halik <sthalik@misaki.pl>
2021-10-04 15:53:26 +0200

    tracker/{aruco,easy,pt,wii}, video/opencv: allow dynamic opencv

--- cmake/opentrack-opencv.cmake
+++ cmake/opentrack-opencv.cmake
@@ -2,13 +2,3 @@ include_guard(GLOBAL)
 
 include(opentrack-boilerplate)
 find_package(OpenCV QUIET)
-
-function(otr_install_opencv_libs)
-    foreach(k core features2d calib3d flann imgcodecs imgproc videoio)
-        otr_install_lib("opencv_${k}" "${opentrack-libexec}")
-    endforeach()
-endfunction()
-
-if(TARGET opencv_core)
-    otr_install_opencv_libs()
-endif()
