d9f0f70ce50a046046cc7c88b992876dac7782b2
Stanislaw Halik <sthalik@misaki.pl>
2021-10-15 13:28:37 +0200

    tracker/trackhat: add stub dialog

--- tracker-trackhat/lang/nl_NL.ts
+++ tracker-trackhat/lang/nl_NL.ts
@@ -2,6 +2,49 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>Form</name>
+    <message>
+        <source>TrackHat tracker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Camera</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Threshold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Exposure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Gain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Min point size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Max point size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> px</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>trackhat_module</name>
     <message>
         <source>TrackHat Point Tracker</source>
