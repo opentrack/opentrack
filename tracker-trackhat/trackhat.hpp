bc5584d601110b5215650d41a693ce64d69ca4cb
Stanislaw Halik <sthalik@misaki.pl>
2021-10-17 15:15:36 +0200

    tracker/trackhat: fix threshold register range

--- tracker-trackhat/trackhat.hpp
+++ tracker-trackhat/trackhat.hpp
@@ -29,7 +29,7 @@ struct trackhat_settings : opts
     trackhat_settings();
     value<slider_value> exposure{b, "exposure", {0x80, 0x10, 0xff}};
     value<slider_value> gain{b, "gain", {16, 0, 47}};
-    value<slider_value> threshold{b, "threshold", {0x97, 64, 0xff}};
+    value<slider_value> threshold{b, "threshold", {0x97, 64, 0xfe}};
     value<slider_value> threshold_2{b, "threshold-2", {0x03, 1, 0xfe}};
     value<model_type> model{b, "model", model_mini_clip_left};
     value<double> min_pt_size{b, "min-point-size", 10};
