opentrack/opentrack

commit 5ec680d09ced3cdf10ae4fe6ee36985dbcba913c

opentrack: fix compatibility option for obsolete QT

spline/spline-widget.cpp
@@ -109,9 +109,11 @@ void spline_widget::drawBackground()
     const int ystep = (int)std::ceil(y_step_), xstep = (int)std::ceil(x_step_);
     const double maxx = config->max_input();
     const double maxy = config->max_output();
-
+#ifndef OTR_OBSOLETE_QT_WORKAROUND
     double space_width = metrics.horizontalAdvance(' ');
-
+#else
+    double space_width = metrics.boundingRect(' ').right();
+#endif
     painter.setPen(palette().text().color());
 
     // vertical grid
@@ -145,8 +147,11 @@ void spline_widget::drawBackground()
 
         const QString text = QString::number(i);
         QRectF rect = metrics.boundingRect(text);
+#ifndef OTR_OBSOLETE_QT_WORKAROUND
         double advance = metrics.horizontalAdvance(text);
-
+#else
+        double advance = metrics.boundingRect(text).right();
+#endif
         painter.drawText(QPointF(x - advance/2 - rect.left(),
                                  pixel_bounds.bottom() + metrics.lineSpacing()),
                          text);

tracker-fusion/fusion.cpp
@@ -14,6 +14,7 @@
 #include <QDebug>
 #include <QMessageBox>
 #include <QApplication>
+#include <cassert>
 
 static const char* own_name = "fusion";
 

commit b3880a052ef103fa9d171158c1ea38a51c220cbd

spline: really fix old Qt workaround
Fallout after 5ec680d09ced3cdf10ae4fe6ee36985dbcba913c

spline/spline-widget.cpp
@@ -14,6 +14,10 @@
 
 #include <QDebug>
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 11, 0)
+#   define OTR_OBSOLETE_QT_WORKAROUND
+#endif
+
 namespace spline_detail {
 
 spline_widget::spline_widget(QWidget *parent) : QWidget(parent)