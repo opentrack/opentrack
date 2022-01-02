0a629d060b000b5c2fc44581b07533fe582c3292
Stanislaw Halik <sthalik@misaki.pl>
2022-01-03 00:20:26 +0100

    github ci: hard way it is

---
+++ .github/workflows/build-windows.bat
@@ -0,0 +1,3 @@
+set path=C:\Program Files\PowerShell\7;D:\a\opentrack\opentrack\ninja-build;C:\Program Files (x86)\pipx_bin;C:\hostedtoolcache\windows\Python\3.7.9\x64\Scripts;C:\hostedtoolcache\windows\Python\3.7.9\x64;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files\CMake\bin;C:\Program Files\Git\cmd;C:\Users\runneradmin\AppData\Local\Microsoft\WindowsApps;%GITHUB_WORKSPACE%/ninja-build
+"C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat" && %*
+exit /b %ERRORLEVEL%
