537c5e0c082ba969ab9fc83a0104fbd90e3c7ce6
Stanislaw Halik <sthalik@misaki.pl>
2022-04-26 18:37:55 +0200

    tracker/pt: make dialog less tall

--- tracker-pt/lang/nl_NL.ts
+++ tracker-pt/lang/nl_NL.ts
@@ -300,6 +300,10 @@ Don&apos;t roll or change position.</source>
         <source>Deadzone</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Filter</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>pt_impl::TrackerDialog_PT</name>
