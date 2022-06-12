cb9e94a3b8d6d950d3b210c79a44fe4192fa4044
Stanislaw Halik <sthalik@misaki.pl>
2022-06-12 09:52:08 +0200

    gui: remove outdated comment

--- gui/init.hpp
+++ gui/init.hpp
@@ -8,8 +8,6 @@
 
 OTR_GUI_EXPORT int otr_main(int argc, char** argv, std::function<std::unique_ptr<QWidget>()> const& make_main_window);
 
-// XXX TODO need split MainWindow into mixins each implementing part of the functionality
-
 template<typename F>
 auto run_application(int argc, char** argv, F&& fun)
 {
