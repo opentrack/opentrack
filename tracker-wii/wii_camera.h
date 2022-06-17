2aee2a94ea53620e3da7f976875ca4101eec780d
Stanislaw Halik <sthalik@misaki.pl>
2022-06-17 11:58:04 +0200

    tracker/wii: clean up a little

--- tracker-wii/wii_camera.h
+++ tracker-wii/wii_camera.h
@@ -27,47 +27,39 @@ namespace pt_module {
 
 struct WIICamera final : pt_camera
 {
-    WIICamera(const QString& module_name);
-    ~WIICamera() override;
+	WIICamera(const QString& module_name);
+	~WIICamera() override;
 
-    bool start(const pt_settings&) override;
-    void stop() override;
+	bool start(const pt_settings&) override;
+	void stop() override;
 
-    result get_frame(pt_frame& Frame) override;
-    result get_info() const override;
+	result get_frame(pt_frame& Frame) override;
+	result get_info() const override;
 
-    pt_camera_info get_desired() const override { return cam_desired; }
-    QString get_desired_name() const override;
-    QString get_active_name() const override;
+	pt_camera_info get_desired() const override { return cam_desired; }
+	QString get_desired_name() const override;
+	QString get_active_name() const override;
 
-    void set_fov(f x) override { (void) x; }
-    void show_camera_settings() override;
+	void set_fov(f x) override { (void) x; }
+	void show_camera_settings() override;
 
 private:
 	std::unique_ptr<wiimote> m_pDev;
 	static void on_state_change(wiimote &remote,
 		state_change_flags changed,
 		const wiimote_state &new_state);
-	bool onExit = false;
 
 	wii_camera_status pair();
 	wii_camera_status get_frame(cv::Mat& Frame);
 	bool get_points(struct wii_info& wii);
 	void get_status(struct wii_info& wii);
 
-    double dt_mean = 0;
+	pt_camera_info cam_info;
+	pt_camera_info cam_desired;
+	int pitch_ = 0, roll_ = 0;
+	pt_settings s;
 
-    Timer t;
-
-    pt_camera_info cam_info;
-    pt_camera_info cam_desired;
-    QString desired_name, active_name;
-
-    pt_settings s;
-
-    int pitch_ = 0, roll_ = 0;
-
-    static constexpr inline double dt_eps = 1./384;
+	static constexpr inline double dt_eps = 1./384;
 };
 
 } // ns pt_module
--- tracker-wii/wii_point_extractor.cpp
+++ tracker-wii/wii_point_extractor.cpp
@@ -27,7 +27,6 @@ using namespace pt_module;
 
 WIIPointExtractor::WIIPointExtractor(const QString& module_name) : s(module_name)
 {
-
 }
 
 //define a temp draw function
@@ -35,8 +34,8 @@ void WIIPointExtractor::draw_point(cv::Mat& preview_frame, const vec2& p, const 
 {
 	static constexpr int len = 9;
 
-    cv::Point p2(iround(p[0] * preview_frame.cols + preview_frame.cols / 2.f),
-                 iround(-p[1] * preview_frame.cols + preview_frame.rows / 2.f));
+	cv::Point p2(iround(p[0] * preview_frame.cols + preview_frame.cols / 2.f),
+		 iround(-p[1] * preview_frame.cols + preview_frame.rows / 2.f));
 
 	cv::line(preview_frame,
 		cv::Point(p2.x - len, p2.y),
@@ -48,7 +47,7 @@ void WIIPointExtractor::draw_point(cv::Mat& preview_frame, const vec2& p, const 
 		cv::Point(p2.x, p2.y + len),
 		color,
 		thickness);
-};
+}
 
 void WIIPointExtractor::draw_points(cv::Mat& preview_frame, const struct wii_info& wii)
 {
@@ -75,11 +74,11 @@ void WIIPointExtractor::draw_points(cv::Mat& preview_frame, const struct wii_inf
 void WIIPointExtractor::draw_bg(cv::Mat& preview_frame, const struct wii_info& wii)
 {
 	//draw battery status
-    cv::line(preview_frame,
-             cv::Point(0, 0),
-             cv::Point(preview_frame.cols*wii.BatteryPercent / 100, 0),
-             (wii.bBatteryDrained ? cv::Scalar(255, 0, 0) : cv::Scalar(0, 140, 0)),
-             2);
+	cv::line(preview_frame,
+		cv::Point(0, 0),
+		cv::Point(preview_frame.cols*wii.BatteryPercent / 100, 0),
+		(wii.bBatteryDrained ? cv::Scalar(255, 0, 0) : cv::Scalar(0, 140, 0)),
+		2);
 
 	//draw horizon
 	int pdelta = iround(preview_frame.rows / 4.f * tan(wii.Pitch * pi / 180.f));
@@ -100,38 +99,38 @@ void WIIPointExtractor::extract_points(const pt_frame& frame_,
 	const struct wii_info& wii = frame_.as_const<WIIFrame>()->wii;
 	cv::Mat& preview_frame = *preview_frame_.as<WIIPreview>();
 
-    map_points(wii, points);
-    if (preview_visible && wii.status == wii_cam_data_change)
-    {
-        draw_bg(preview_frame, wii);
-        draw_points(preview_frame, wii);
-    }
+	map_points(wii, points);
+	if (preview_visible && wii.status == wii_cam_data_change)
+	{
+		draw_bg(preview_frame, wii);
+		draw_points(preview_frame, wii);
+	}
 }
 
 bool WIIPointExtractor::map_points(const struct wii_info& wii, std::vector<vec2>& points)
 {
-    constexpr int W = 1024;
-    constexpr int H = 768;
-    points.reserve(4);
-    points.clear();
+	constexpr int W = 1024;
+	constexpr int H = 768;
+	points.reserve(4);
+	points.clear();
 
-    for (unsigned index = 0; index < 4; index++) // NOLINT(modernize-loop-convert)
-    {
-        const struct wii_info_points &dot = wii.Points[index];
-        if (dot.bvis) {
-            //qDebug() << "wii:" << dot.RawX << "+" << dot.RawY;
-            //anti-clockwise rotate the 2D point
-            const float RX = W - dot.ux;
-            const float RY = H - dot.uy;
-            //vec2 dt((dot.RawX - W / 2.0f) / W, -(dot.RawY - H / 2.0f) / W);
-            //vec2 dt((RX - W / 2.0f) / W, -(RY - H / 2.0f) / W);
-            //vec2 dt((2.0f*RX - W) / W, -(2.0f*RY - H ) / W);
-            vec2 dt;
-            std::tie(dt[0], dt[1]) = to_screen_pos(RX, RY, W, H);
+	for (unsigned index = 0; index < 4; index++) // NOLINT(modernize-loop-convert)
+	{
+		const struct wii_info_points &dot = wii.Points[index];
+		if (dot.bvis) {
+			//qDebug() << "wii:" << dot.RawX << "+" << dot.RawY;
+			//anti-clockwise rotate the 2D point
+			const float RX = W - dot.ux;
+			const float RY = H - dot.uy;
+			//vec2 dt((dot.RawX - W / 2.0f) / W, -(dot.RawY - H / 2.0f) / W);
+			//vec2 dt((RX - W / 2.0f) / W, -(RY - H / 2.0f) / W);
+			//vec2 dt((2.0f*RX - W) / W, -(2.0f*RY - H ) / W);
+			vec2 dt;
+			std::tie(dt[0], dt[1]) = to_screen_pos(RX, RY, W, H);
 
-            points.push_back(dt);
-        }
-    }
+			points.push_back(dt);
+		}
+	}
 
-    return points.size() >= PointModel::N_POINTS;
+	return points.size() >= PointModel::N_POINTS;
 }
