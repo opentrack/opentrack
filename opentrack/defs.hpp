8ff5c1a349781810a9166128e7840e0ee281e8ae
Stanislaw Halik <sthalik@misaki.pl>
2023-01-04 13:06:53 +0100

    [MERGE] opentrack: add UI_NO_VIDEO_FRAME

--- opentrack/defs.hpp
+++ opentrack/defs.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
-//#define UI_FORCED_TRACKER "pt"
-//#define UI_FORCED_FILTER "accela"
+#define UI_FORCED_TRACKER "neuralnet"
+#define UI_FORCED_FILTER "accela"
 
-//#define UI_NO_TRACKER_COMBOBOX
-//#define UI_NO_FILTER_COMBOBOX
+#define UI_NO_TRACKER_COMBOBOX
+#define UI_NO_FILTER_COMBOBOX
 
-//#define UI_NO_TRACKER_SETTINGS_BUTTON
-//#define UI_NO_FILTER_SETTINGS_BUTTON
+#define UI_NO_TRACKER_SETTINGS_BUTTON
+#define UI_NO_FILTER_SETTINGS_BUTTON
 
-//#define UI_NO_RAW_DATA
-//#define UI_NO_GAME_DATA
+#define UI_NO_RAW_DATA
+#define UI_NO_GAME_DATA
+//#define UI_NO_VIDEO_FEED
+
 //define UI_ACCELA_OLD_STAIRCASE
+
