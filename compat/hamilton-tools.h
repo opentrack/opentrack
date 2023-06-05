91c278657ee5e424dcc1b7c83d078fde18cf8c55
Tom Brazier <tom_github@firstsolo.net>
2023-06-05 15:02:07 +0100

    Export hamilton tools functions

--- compat/hamilton-tools.h
+++ compat/hamilton-tools.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "export.hpp"
 #include "compat/math.hpp"
+
 constexpr double TO_RAD = (M_PI / 180);
 constexpr double TO_DEG = (180 / M_PI);
 constexpr double EPSILON = 1e-30;
@@ -19,9 +21,9 @@ struct tQuat
         {x = X; y = Y; z = Z; w = W;}
 };
 
-double  VectorDistance(const double v1[], const tVector v2);
-tVector Lerp          (const tVector s, const double d[], const double alpha);
-tQuat   QuatFromYPR   (const double YPR[]);
-double  AngleBetween  (const tQuat S, const tQuat D);
-tQuat   Slerp         (const tQuat S, const tQuat D, const double alpha);
-void    QuatToYPR     (const tQuat Q, double YPR[]);
+double  OTR_COMPAT_EXPORT VectorDistance(const double v1[], const tVector v2);
+tVector OTR_COMPAT_EXPORT Lerp          (const tVector s, const double d[], const double alpha);
+tQuat   OTR_COMPAT_EXPORT QuatFromYPR   (const double YPR[]);
+double  OTR_COMPAT_EXPORT AngleBetween  (const tQuat S, const tQuat D);
+tQuat   OTR_COMPAT_EXPORT Slerp         (const tQuat S, const tQuat D, const double alpha);
+void    OTR_COMPAT_EXPORT QuatToYPR     (const tQuat Q, double YPR[]);
