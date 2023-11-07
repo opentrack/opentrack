7af99f4a6a51a291dbe3d59156e6b9203e4eb3dd
前进! <92249100+ddzzkunye@users.noreply.github.com>
2023-11-07 10:07:12 +0800

    Some translations

--- tracker-tobii/lang/zh_CN.ts
+++ tracker-tobii/lang/zh_CN.ts
@@ -16,7 +16,7 @@
     </message>
     <message>
         <source>Please make sure the Tobii Experience application is running and tracking is active.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">请确保 Tobii 应用程序正在运行，跟踪功能处于激活状态. </translation>
     </message>
 </context>
 </TS>
