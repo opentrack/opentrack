5ac9e29051030ca2403a86bd0c5f90aa3ce78d40
Stanislaw Halik <sthalik@misaki.pl>
2024-02-08 15:41:25 +0100

    cmake: kill obsolete variant stuff
    This isn't needed for TrackHat as we switched to a different method.

--- cmake/opentrack-variant.cmake
+++ cmake/opentrack-variant.cmake
@@ -1,29 +1,32 @@
 include_guard(GLOBAL)
 
-function(otr_dist_select_variant)
-    file(GLOB variants CONFIGURE_DEPENDS "variant/*")
+function(otr_init_variant)
+    set_property(GLOBAL PROPERTY opentrack-variant "default")
+    set_property(GLOBAL PROPERTY opentrack-ident "opentrack-2.3")
 
-    set(variant-list "")
-
-    foreach(k ${variants})
-        get_filename_component(name "${k}" NAME)
-        if(EXISTS "${k}/_variant.cmake")
-            list(APPEND variant-list "${name}")
-        else()
-            message(FATAL_ERROR "stray variant dir '${name}'")
-        endif()
-    endforeach()
-
-    set(opentrack_variant "default" CACHE STRING "")
-    set(dir "${CMAKE_SOURCE_DIR}/variant/${opentrack_variant}")
-
-    if(NOT EXISTS "${dir}/_variant.cmake")
-        set(opentrack_variant "default" CACHE STRING "" FORCE)
-        set(dir "${CMAKE_SOURCE_DIR}/variant/${opentrack_variant}")
-    endif()
-
-    set_property(CACHE opentrack_variant PROPERTY STRINGS "${variant-list}")
-
-    include("${dir}/_variant.cmake")
-    otr_init_variant()
+    set(subprojects
+        "tracker-*"
+        "proto-*"
+        "filter-*"
+        "options"
+        "api"
+        "compat"
+        "logic"
+        "dinput"
+        "gui"
+        "main"
+        "x-plane-plugin"
+        "csv"
+        "pose-widget"
+        "spline"
+        "qxt-mini"
+        "macosx"
+        "cv"
+        "migration"
+        "main-window"
+        "video"
+        "video-*"
+        "opentrack"
+    )
+    set_property(GLOBAL PROPERTY opentrack-subprojects "${subprojects}")
 endfunction()
