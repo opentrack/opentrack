5308a459e8c575457ef1c024e306b0716b1ae5d7
Stanislaw Halik <sthalik@misaki.pl>
2025-04-28 19:40:10 +0200

    i18n: add missing German translations

---
+++ proto-mouse/lang/de_DE.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>UI_mouse</name>
+    <message>
+        <source>Mouse protocol</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>X axis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>X</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Y</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Z</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yaw</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pitch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Roll</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sensitivity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Y axis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Direct input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Legacy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>mouse</name>
+    <message>
+        <source>Mouse tracker</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>mouseDll</name>
+    <message>
+        <source>mouse emulation</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ tracker-tobii/lang/de_DE.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>tobii_metadata</name>
+    <message>
+        <source>Tobii Eye Tracker</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>tobii_ui</name>
+    <message>
+        <source>Tobii Eye Tracker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please make sure the Tobii Experience application is running and tracking is active.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
