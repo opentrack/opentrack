8b043bfdc75143670e23a1bb189c62ab499ce270
Stanislaw Halik <sthalik@misaki.pl>
2025-04-28 19:40:10 +0200

    i18n: add missing German translations

---
+++ proto-vjoystick/lang/de_DE.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>vjoystick</name>
+    <message>
+        <source>vjoystick</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Go to the &lt;a href=&quot;https://github.com/jshafer817/vJoy&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;vJoy&lt;/span&gt;&lt;/a&gt; project site or &lt;a href=&quot;https://github.com/jshafer817/vJoy/releases/tag/v2.1.9.1&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;download directly&lt;/span&gt;&lt;/a&gt; for Windows 10 and 11.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>vjoystick won&apos;t work without the driver installed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>driver/SDK version mismatch (dll 0x%1, driver 0x%2)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>BUG: handle leak.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Virtual joystick already in use.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Device missing. Add joystick #1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unknown error.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unknown error #%1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>vjoystick driver problem</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Download the driver</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Visit project site</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Driver problem.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Virtual joystick</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>vjoystick_metadata</name>
+    <message>
+        <source>Joystick emulation -- vjoystick</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
